93746f330f799f3e7f1b28bfe4d302e0ea4c6be9
FatSaw <eklesosek@gmail.com>
2023-06-05 05:15:35 +0300

    Update LangOptions.java

--- src/me/bomb/amusic/LangOptions.java
+++ src/me/bomb/amusic/LangOptions.java
@@ -17,7 +17,8 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 
 enum LangOptions {loadmusic_usage,loadmusic_nopermission,loadmusic_nopermissionother,loadmusic_noconsoleselector,loadmusic_targetoffline,loadmusic_noplaylist,loadmusic_loaderunavilable,loadmusic_success,playmusic_usage,playmusic_nopermission,playmusic_nopermissionother,playmusic_noconsoleselector,playmusic_targetoffline,playmusic_noplaylist,playmusic_missingtrack,playmusic_playing,playmusic_success,playmusic_stopping,playmusic_stop,repeat_usage,repeat_nopermission,repeat_nopermissionother,repeat_noconsoleselector,repeat_targetoffline,repeat_unknownrepeattype,repeat_repeatall,repeat_repeatone,repeat_playall,repeat_playone,repeat_random;
@@ -50,23 +51,26 @@ enum LangOptions {loadmusic_usage,loadmusic_nopermission,loadmusic_nopermissiono
 			aavilablelocales = deffaultconfigurationsection.getKeys(false);
 			aavilablelocales.add("default");
 			for(LangOptions lang : values()) {
-				lang.text = new HashMap<String,String>();
 				for(String locale : aavilablelocales) {
-					String msg = alang.getString(locale.concat(".").concat(lang.toString().replaceAll("_", ".")),"[{\"text\":\"ERROR LANG OPTION \",\"bold\":true,\"color\":\"dark_red\"},{\"text\":\"".concat(lang.toString()).concat("\",\"bold\":true,\"color\":\"red\"},{\"text\":\" FOR LOCALE \",\"bold\":true,\"color\":\"dark_red\"},{\"text\":\"").concat(locale).concat("\",\"bold\":true,\"color\":\"red\"},{\"text\":\" NOT EXSIST\",\"bold\":true,\"color\":\"dark_red\"}]"));
+					String optionpath = locale.concat(".").concat(lang.name().replaceAll("_", "."));
+					String msg = alang.getString(optionpath);
+					if(msg.isEmpty()) {
+						msg = TextComponent.toLegacyText(ComponentSerializer.parse("[{\"text\":\"ERROR LANG OPTION \",\"bold\":true,\"color\":\"dark_red\"},{\"text\":\"".concat(lang.name()).concat("\",\"bold\":true,\"color\":\"red\"},{\"text\":\" FOR LOCALE \",\"bold\":true,\"color\":\"dark_red\"},{\"text\":\"").concat(locale).concat("\",\"bold\":true,\"color\":\"red\"},{\"text\":\" NOT EXSIST\",\"bold\":true,\"color\":\"dark_red\"}]")));
+					} else {
+						char startchar = msg.charAt(0),endchar = msg.charAt(msg.length()-1);
+						if(startchar=='['&&endchar==']'||startchar=='{'&&endchar=='}') {
+							msg = TextComponent.toLegacyText(ComponentSerializer.parse(msg));
+						}
+					}
 					lang.text.put(locale, msg);
 				}
 			}
 		}
 		avilablelocales = aavilablelocales;
 	}
-	private static final BaseComponent[] langnotloaded = ComponentSerializer.parse("{\"text\":\"ERROR LANG FILE NOT LOADED\",\"bold\":true,\"color\":\"dark_red\"}");
 	private static final Set<String> avilablelocales;
-	private Map<String,String> text = null;
+	private final Map<String,String> text = new HashMap<String,String>();
 	protected void sendMsg(CommandSender target,Placeholders...placeholders) {
-		if(text==null) {
-			target.sendMessage(langnotloaded);
-			return;
-		}
 		String msg = text.get("default");
 		if(target instanceof Player) {
 			String locale = getLocale((Player)target);
@@ -78,44 +82,21 @@ enum LangOptions {loadmusic_usage,loadmusic_nopermission,loadmusic_nopermissiono
 			for(Placeholders placeholder:placeholders) {
 				msg = msg.replaceAll(placeholder.placeholder, placeholder.value);
 			}
-			target.sendMessage(ComponentSerializer.parse(msg));
+			target.sendMessage(msg);
 		}
 	}
 	protected void sendMsgActionbar(Player target,Placeholders...placeholders) {
-		if(text==null) {
-			target.sendActionBar(ComponentSerializer.toString(langnotloaded));
-			return;
-		}
 		String locale = getLocale(target);
 		String msg = avilablelocales.contains(locale)?text.get(locale):text.get("default");
 		if(!msg.isEmpty()) {
 			for(Placeholders placeholder:placeholders) {
 				msg = msg.replaceAll(placeholder.placeholder, placeholder.value);
 			}
-			target.sendActionBar(BaseComponent.toLegacyText(ComponentSerializer.parse(msg)));
+			target.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(msg));
 		}
 	}
 	private static String getLocale(Player player) {
-		switch (Bukkit.getServer().getClass().getPackage().getName().substring(23)) {
-		case "v1_16_R3":
-			return ((org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer) player).getHandle().locale.toLowerCase();
-		case "v1_15_R1":
-			return ((org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer) player).getHandle().locale.toLowerCase();
-		case "v1_14_R1":
-			return ((org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer) player).getHandle().locale.toLowerCase();
-		case "v1_13_R2":
-			return ((org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer) player).getHandle().locale.toLowerCase();
-		case "v1_12_R1":
-			return ((org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer) player).getHandle().locale.toLowerCase();
-		case "v1_11_R1":
-			return ((org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer) player).getHandle().locale.toLowerCase();
-		case "v1_10_R1":
-			return ((org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer) player).getHandle().locale.toLowerCase();
-		case "v1_9_R2":
-			return ((org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer) player).getHandle().locale.toLowerCase();
-		default:
-			return player.getLocale().toLowerCase();
-		}
+		return player.spigot().getLocale().toLowerCase();
 	}
 	protected static class Placeholders {
 		protected final String placeholder;

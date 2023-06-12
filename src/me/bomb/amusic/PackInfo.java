4854fff3b538efc728ba438a7e6774504e4e4736
FatSaw <eklesosek@gmail.com>
2023-06-12 06:47:07 +0300

    Create PackInfo.java

---
+++ src/me/bomb/amusic/PackInfo.java
@@ -0,0 +1,12 @@
+package me.bomb.amusic;
+
+import java.util.List;
+
+class PackInfo {
+	protected final List<String> songs;
+	protected final List<Short> lengths;
+    protected PackInfo(List<String> songs,List<Short> lengths) {
+    	this.songs = songs;
+    	this.lengths = lengths;
+    }
+}

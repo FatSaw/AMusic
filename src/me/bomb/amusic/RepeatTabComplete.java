64a3429fe9e951f5e1885dd7f966497714c40a6c
FatSaw <eklesosek@gmail.com>
2023-06-12 06:47:36 +0300

    Update RepeatTabComplete.java

--- src/me/bomb/amusic/RepeatTabComplete.java
+++ src/me/bomb/amusic/RepeatTabComplete.java
@@ -9,7 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-class RepeatTabComplete implements TabCompleter {
+final class RepeatTabComplete implements TabCompleter {
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
 		if(!sender.hasPermission("amusic.repeat")) {

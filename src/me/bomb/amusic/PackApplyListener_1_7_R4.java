89ab46f165d800aab06a369735e80397487a10dd
FatSaw <eklesosek@gmail.com>
2023-12-12 23:10:26 +0200

    Update PackApplyListener_1_7_R4.java

--- src/me/bomb/amusic/PackApplyListener_1_7_R4.java
+++ src/me/bomb/amusic/PackApplyListener_1_7_R4.java
@@ -13,21 +13,21 @@ import net.minecraft.util.io.netty.channel.ChannelPipeline;
 
 final class PackApplyListener_1_7_R4 extends PackApplyListener {
 	
+	private final Field channelField;
+	
+	protected PackApplyListener_1_7_R4() throws NoSuchFieldException, SecurityException {
+		channelField = NetworkManager.class.getDeclaredField("m");
+		channelField.setAccessible(true);
+	}
+	
 	@Override
 	protected void addApplyListenTask(Player player) {
 		EntityPlayer entityplayer = ((CraftPlayer)player).getHandle();
 		ChannelPipeline pipeline = null;
 		try {
-			Field channelField = NetworkManager.class.getDeclaredField("m");
-		    channelField.setAccessible(true);
-		    Channel channel = (Channel) channelField.get(entityplayer.playerConnection.networkManager);
-		    channelField.setAccessible(false);
+			Channel channel = (Channel) channelField.get(entityplayer.playerConnection.networkManager);
 		    pipeline = channel.pipeline();
-		    
-		} catch (NoSuchFieldException e) {
-		} catch (SecurityException e) {
-		} catch (IllegalArgumentException e) {
-		} catch (IllegalAccessException e) {
+		} catch (IllegalArgumentException | IllegalAccessException e) {
 		}
 		if(pipeline==null) {
 			return;
@@ -42,15 +42,8 @@ final class PackApplyListener_1_7_R4 extends PackApplyListener {
 		EntityPlayer entityplayer = ((CraftPlayer)player).getHandle();
 		Channel achannel = null;
 		try {
-			Field channelField = NetworkManager.class.getDeclaredField("m");
-		    channelField.setAccessible(true);
-		    achannel = (Channel) channelField.get(entityplayer.playerConnection.networkManager);
-		    channelField.setAccessible(false);
-		    
-		} catch (NoSuchFieldException e) {
-		} catch (SecurityException e) {
-		} catch (IllegalArgumentException e) {
-		} catch (IllegalAccessException e) {
+			achannel = (Channel) channelField.get(entityplayer.playerConnection.networkManager);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
 		}
 		if(achannel==null) {
 			return;

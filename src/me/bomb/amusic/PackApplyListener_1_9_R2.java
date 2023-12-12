7c3978bae9f96a18dcf17c22f7e3309d5a5f652c
FatSaw <eklesosek@gmail.com>
2023-12-12 06:25:44 +0200

    Create PackApplyListener_1_9_R2.java

---
+++ src/me/bomb/amusic/PackApplyListener_1_9_R2.java
@@ -0,0 +1,34 @@
+package me.bomb.amusic;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelPipeline;
+import net.minecraft.server.v1_9_R2.EntityPlayer;
+
+final class PackApplyListener_1_9_R2 extends PackApplyListener {
+	
+	@Override
+	protected void addApplyListenTask(Player player) {
+		EntityPlayer entityplayer = ((CraftPlayer)player).getHandle();
+		ChannelPipeline pipeline = entityplayer.playerConnection.networkManager.channel.pipeline();
+		AtomicBoolean ab = new AtomicBoolean(true);
+		pipeline.addBefore("packet_handler", "applylistener", new PacketMonitor_1_9_R2(ab));
+		applylisteners.put(player.getUniqueId(), ab);
+	}
+
+	@Override
+	protected void removeApplyListenTask(Player player) {
+		EntityPlayer entityplayer = ((CraftPlayer)player).getHandle();
+		Channel channel = entityplayer.playerConnection.networkManager.channel;
+		channel.eventLoop().submit(() -> {
+			channel.pipeline().remove("applylistener");
+			return null;
+		});
+		applylisteners.remove(player.getUniqueId());
+	}
+
+}

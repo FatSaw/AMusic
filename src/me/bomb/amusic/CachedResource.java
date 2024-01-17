3f2c6110172426387cfe72d4bf4b66a55293f303
FatSaw <eklesosek@gmail.com>
2024-01-17 05:40:32 +0200

    Update CachedResource.java

--- src/me/bomb/amusic/CachedResource.java
+++ src/me/bomb/amusic/CachedResource.java
@@ -50,7 +50,6 @@ final class CachedResource {
 			return;
 		}
 		accepted.add(targets.get(targetplayer));
-		PackApplyListener.reset(targetplayer);
 	}
 
 	protected static boolean waitAcception(UUID token) {
@@ -66,7 +65,7 @@ final class CachedResource {
 				for (UUID target : targets.keySet()) {
 					if (targets.get(target).equals(token)) {
 						targets.remove(target);
-						PackApplyListener.reset(target);
+						//PackApplyListener.reset(target);
 					}
 				}
 			}

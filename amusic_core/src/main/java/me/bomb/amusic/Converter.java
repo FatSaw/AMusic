48b56db40403745602131090156cf5172fb36983
FatSaw <eklesosek@gmail.com>
2024-02-26 07:35:30 +0200

    Update Converter.java

--- amusic_core/src/main/java/me/bomb/amusic/Converter.java
+++ amusic_core/src/main/java/me/bomb/amusic/Converter.java
@@ -5,46 +5,37 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 final class Converter implements Runnable {
-	protected final AtomicBoolean status;
-	protected final File input, output;
-
-	private static final Pattern SUCCESS_PATTERN = Pattern.compile("^\\s*video\\:\\S+\\s+audio\\:\\S+\\s+subtitle\\:\\S+\\s+global headers\\:\\S+.*$", Pattern.CASE_INSENSITIVE);
-	private static final Pattern PROGRESS_INFO_PATTERN = Pattern.compile("\\s*(\\w+)\\s*=\\s*(\\S+)\\s*", Pattern.CASE_INSENSITIVE);
-
-	private int step = 0;
-	private String lastWarning = null;
-	private final List<String> unhandledMessages = new LinkedList<>();
-
-	private Process ffmpeg = null;
-
-	private Thread ffmpegKiller = null;
-
-	private InputStream inputStream = null;
-
-	private OutputStream outputStream = null;
-
-	private InputStream errorStream = null;
 	
-	private static final String fmpegbinarypath, binarywithargs;
+	private static final Pattern SUCCESS_PATTERN, PROGRESS_INFO_PATTERN;
+	private static final String fmpegbinarypath;
+	
+	private final AtomicBoolean status;
+	protected final File input, output;
+	
+	//private final List<String> unhandledMessages = new LinkedList<>();
+	
+	private final int bitrate, samplingrate;
+	private final byte channels;
 	
 	static {
+		SUCCESS_PATTERN = Pattern.compile("^\\s*video\\:\\S+\\s+audio\\:\\S+\\s+subtitle\\:\\S+\\s+global headers\\:\\S+.*$", Pattern.CASE_INSENSITIVE);
+		PROGRESS_INFO_PATTERN = Pattern.compile("\\s*(\\w+)\\s*=\\s*(\\S+)\\s*", Pattern.CASE_INSENSITIVE);
 		String os = System.getProperty("os.name").toLowerCase();
 		fmpegbinarypath = new File("plugins/AMusic/", "ffmpeg".concat(os.contains("windows") ? ".exe" : os.contains("mac") ? "-osx" : "")).getAbsolutePath();
-		binarywithargs = " -acodec libvorbis -ab ".concat(Integer.toString(64000)).concat(" -ac ").concat(Integer.toString(2)).concat(" -ar ").concat(Integer.toString(44100)).concat(" -f ogg");
 	}
 
-	protected Converter(boolean async, File input, File output) {
+	protected Converter(boolean async,int bitrate, byte channels, int samplingrate ,File input, File output) {
 		this.input = input;
 		this.output = output.getAbsoluteFile();
+		this.bitrate = bitrate;
+		this.channels = channels;
+		this.samplingrate = samplingrate;
 		if (async) {
 			status = new AtomicBoolean(false);
 			new Thread(this).start();
@@ -56,158 +47,140 @@ final class Converter implements Runnable {
 
 	@Override
 	public void run() {
+		StringBuilder sb = new StringBuilder(fmpegbinarypath);
+		sb.append(" -i ");
+		sb.append(input.getAbsolutePath());
+		sb.append(" -acodec libvorbis -ab ");
+		sb.append(Integer.toString(bitrate));
+		sb.append(" -ac ");
+		sb.append(Byte.toString(channels));
+		sb.append(" -ar ");
+		sb.append(Integer.toString(samplingrate));
+		sb.append(" -f ogg -y ");
+		sb.append(output.getAbsolutePath());
+		String cmd = sb.toString();
+		Runtime runtime = Runtime.getRuntime();
+		Process ffmpeg = null;
 		try {
-			try {
-
-				Runtime runtime = Runtime.getRuntime();
-				StringBuffer sb = new StringBuffer(fmpegbinarypath);
-				sb.append(" -i ");
-				sb.append(input.getAbsolutePath());
-				sb.append(binarywithargs);
-				sb.append(" -y ");
-				sb.append(output.getAbsolutePath());
-				ffmpeg = runtime.exec(sb.toString());
-
-				ffmpegKiller = new ProcessKiller(ffmpeg);
-				runtime.addShutdownHook(ffmpegKiller);
-
-				inputStream = ffmpeg.getInputStream();
-				outputStream = ffmpeg.getOutputStream();
-				errorStream = ffmpeg.getErrorStream();
-
-			} catch (IOException e) {
-				if (inputStream != null) {
-					try {
-						inputStream.close();
-					} catch (Throwable t) {
+			ffmpeg = runtime.exec(cmd);
+		} catch (SecurityException e) {
+			
+			return;
+		} catch(IOException e) {
+			return;
+		}
+		
+		if(ffmpeg == null) {
+			return;
+		}
+		
+		ProcessKiller ffmpegKiller = new ProcessKiller(ffmpeg);
+		runtime.addShutdownHook(ffmpegKiller);
+		
+		InputStream errorStream = ffmpeg.getErrorStream();
+		
+		if(errorStream==null) {
+			ffmpeg.destroy();
+			return;
+		}
+		
+		try {
+			String line;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(errorStream));
+			byte step = 0;
+			String lastWarning = null;
+			while ((line = reader.readLine()) != null) {
+				if (!line.startsWith("Press [q]")) {
+					switch (step) {
+					case 0: {
+						if (line.startsWith("Input #0")) {
+							step = 1;
+						}
 					}
-					inputStream = null;
-				}
-
-				if (outputStream != null) {
-					try {
-						outputStream.close();
-					} catch (Throwable t) {
+						break;
+					case 1: {
+						if (line.startsWith("Stream mapping:") || line.startsWith("Output #0")) {
+							step = 2;
+						} else if (!line.startsWith("  ")) {
+							//unhandledMessages.add(line);
+						}
 					}
-					outputStream = null;
-				}
-
-				if (errorStream != null) {
-					try {
-						errorStream.close();
-					} catch (Throwable t) {
+						break;
+					case 2: {
+						if (line.startsWith("Output #0") || line.startsWith("Stream mapping:")) {
+							step = 3;
+						} else if (!line.startsWith("  ")) {
+							//unhandledMessages.add(line);
+						}
 					}
-					errorStream = null;
-				}
-
-				if (ffmpeg != null) {
-					ffmpeg.destroy();
-					ffmpeg = null;
-				}
-
-				if (ffmpegKiller != null) {
-					Runtime runtime = Runtime.getRuntime();
-					runtime.removeShutdownHook(ffmpegKiller);
-					ffmpegKiller = null;
-				}
-				return;
-			}
-
-			try {
-				String line;
-				BufferedReader reader = new BufferedReader(new InputStreamReader(errorStream));
-				while ((line = reader.readLine()) != null) {
-					if (!line.startsWith("Press [q]")) {
-						switch (step) {
-						case 0: {
-							if (line.startsWith("Input #0")) {
-								step = 1;
-							}
+						break;
+					case 3: {
+						if (line.startsWith("  ")) {
+							
+						} else if (line.startsWith("video:")) {
+							step = 4;
+						} else if (line.startsWith("frame=")) {
+							
+						} else if (line.startsWith("size=")) {
+							
+						} else if (line.endsWith("Queue input is backward in time") || line.contains("Application provided invalid, non monotonically increasing dts to muxer in stream")) {
+							
+						} else {
+							//unhandledMessages.add(line);
 						}
-							break;
-						case 1: {
-							if (line.startsWith("Stream mapping:") || line.startsWith("Output #0")) {
-								step = 2;
-							} else if (!line.startsWith("  ")) {
-								unhandledMessages.add(line);
-							}
-						}
-							break;
-						case 2: {
-							if (line.startsWith("Output #0") || line.startsWith("Stream mapping:")) {
-								step = 3;
-							} else if (!line.startsWith("  ")) {
-								unhandledMessages.add(line);
-							}
-						}
-							break;
-						case 3: {
-							if (line.startsWith("  ")) {
-								
-							} else if (line.startsWith("video:")) {
-								step = 4;
-							} else if (line.startsWith("frame=")) {
-								
-							} else if (line.startsWith("size=")) {
-								
-							} else if (line.endsWith("Queue input is backward in time") || line.contains("Application provided invalid, non monotonically increasing dts to muxer in stream")) {
-								
-							} else {
-								unhandledMessages.add(line);
-							}
-						}
-						}
-						if (line.startsWith("frame=") || line.startsWith("size=")) {
-							try {
-								line = line.trim();
-								if (line.length() > 0) {
-									HashMap<String, String> table = null;
-									Matcher m = PROGRESS_INFO_PATTERN.matcher(line);
-									while (m.find()) {
-										if (table == null) {
-											table = new HashMap<>();
-										}
-										String key = m.group(1);
-										String value = m.group(2);
-										table.put(key, value);
-									}
+					}
+					}
+					if (line.startsWith("frame=") || line.startsWith("size=")) {
+						try {
+							line = line.trim();
+							if (line.length() > 0) {
+								HashMap<String, String> table = null;
+								Matcher m = PROGRESS_INFO_PATTERN.matcher(line);
+								while (m.find()) {
 									if (table == null) {
-										lastWarning = line;
-									} else {
-										lastWarning = null;
+										table = new HashMap<>();
 									}
+									String key = m.group(1);
+									String value = m.group(2);
+									table.put(key, value);
 								}
-							} catch (Exception ex) {
+								if (table == null) {
+									lastWarning = line;
+								} else {
+									lastWarning = null;
+								}
 							}
+						} catch (Exception ex) {
 						}
 					}
 				}
-				if (lastWarning != null) {
-					if (!SUCCESS_PATTERN.matcher(lastWarning).matches()) {
-						return;
-					}
-				}
-				try {
-					ffmpeg.waitFor();
-				} catch (InterruptedException ex) {
-				}
-				if (ffmpeg.exitValue() != 0) {
+			}
+			if (lastWarning != null) {
+				if (!SUCCESS_PATTERN.matcher(lastWarning).matches()) {
 					return;
 				}
-			} catch (IOException e) {
-				return;
-			} finally {
-				if (ffmpeg != null) {
-					ffmpeg.destroy();
-				}
-				ffmpeg = null;
 			}
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
+			try {
+				ffmpeg.waitFor();
+			} catch (InterruptedException ex) {
+			}
+			if (ffmpeg.exitValue() != 0) {
+				return;
+			}
+		} catch (IOException e) {
+			return;
+		} finally {
+			ffmpeg.destroy();
+			ffmpeg = null;
 		}
+		
 		if (status != null) {
 			status.set(true);
 		}
+	}
+	
+	protected boolean finished() {
+		return status == null || status.get();
 	}
 
 	private final class ProcessKiller extends Thread {

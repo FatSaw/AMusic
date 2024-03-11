3afd713a2e51e1b44671650d79e32719df5c0454
FatSaw <eklesosek@gmail.com>
2024-03-11 02:08:44 +0200

    Update AMusicBukkit.java

--- amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/AMusicBukkit.java
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/AMusicBukkit.java
@@ -2,19 +2,17 @@ package me.bomb.amusic.bukkit;
 
 import java.io.File;
 import java.net.InetAddress;
-import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.bomb.amusic.AMusic;
 import me.bomb.amusic.ConfigOptions;
 import me.bomb.amusic.PackSender;
 import me.bomb.amusic.PositionTracker;
-import me.bomb.amusic.SoundStopper;
-import me.bomb.amusic.SoundStarter;
 import me.bomb.amusic.bukkit.command.LoadmusicCommand;
 import me.bomb.amusic.bukkit.command.LoadmusicTabComplete;
 import me.bomb.amusic.bukkit.command.PlaymusicCommand;
@@ -27,29 +25,37 @@ import me.bomb.amusic.resourceserver.ResourceServer;
 
 public final class AMusicBukkit extends JavaPlugin {
 	
-	private static ResourceServer server;
-	private ConfigOptions configoptions;
-	private Data data;
-	private ResourceManager resourcemanager;
-	private PositionTracker positiontracker;
-	private PackSender packsender;
+	private final ConfigOptions configoptions;
+	private final Data data;
+	private final ResourceManager resourcemanager;
+	private final ConcurrentHashMap<Object,InetAddress> playerips;
+	private final PackSender packsender;
+	private final ResourceServer server;
+	private final PositionTracker positiontracker;
+	
+	public AMusicBukkit() {
+		byte ver = Byte.valueOf(Bukkit.getServer().getClass().getPackage().getName().substring(23).split("_", 3)[1]);
+		configoptions = new ConfigOptions(this.getDataFolder(), ver);
+		playerips = new ConcurrentHashMap<Object,InetAddress>(16,0.75f,1);
+		File datafile = new File(this.getDataFolder(), "data.yml");
+		data = new Data(datafile);
+		data.load();
+		if(!datafile.exists()) {
+			data.save();
+		}
+		packsender = new BukkitPackSender();
+		resourcemanager = new ResourceManager(configoptions.maxpacksize, configoptions.servercache, configoptions.clientcache, configoptions.tokensalt);
+		positiontracker = new PositionTracker(new BukkitSoundStarter(), new BukkitSoundStopper(), configoptions.hasplaceholderapi);
+		server = new ResourceServer(playerips, configoptions.port, resourcemanager);
+		try {
+			new AMusic(configoptions, positiontracker, resourcemanager, packsender, data);
+		} catch (ExceptionInInitializerError e) {
+			e.printStackTrace();
+		}
+	}
 	
 	//PLUGIN INIT START
 	public void onEnable() {
-		byte ver = Byte.valueOf(Bukkit.getServer().getClass().getPackage().getName().substring(23).split("_", 3)[1]);
-		configoptions = new ConfigOptions(this.getDataFolder(), ver);
-		ConcurrentHashMap<Object,InetAddress> playerips = new ConcurrentHashMap<Object,InetAddress>(16,0.75f,1);
-		data = new Data(new File(this.getDataFolder(), "data.yml"));
-		data.load();
-		SoundStopper soundstopper;
-		soundstopper = new BukkitSoundStopper();
-		packsender = new BukkitPackSender();
-		SoundStarter soundstarter = new BukkitSoundStarter();
-		resourcemanager = new ResourceManager(configoptions.maxpacksize, configoptions.servercache, configoptions.clientcache, configoptions.tokensalt);
-		positiontracker = new PositionTracker(soundstarter, soundstopper, configoptions.hasplaceholderapi);
-		
-		server = new ResourceServer(playerips, configoptions.port, resourcemanager);
-		
 		PluginCommand loadmusiccommand = getCommand("loadmusic");
 		loadmusiccommand.setExecutor(new LoadmusicCommand(configoptions, data, resourcemanager, positiontracker, packsender));
 		loadmusiccommand.setTabCompleter(new LoadmusicTabComplete(data));
@@ -59,11 +65,14 @@ public final class AMusicBukkit extends JavaPlugin {
 		PluginCommand repeatcommand = getCommand("repeat");
 		repeatcommand.setExecutor(new RepeatCommand(positiontracker));
 		repeatcommand.setTabCompleter(new RepeatTabComplete());
+		playerips.clear();
+		for(Player player : Bukkit.getOnlinePlayers()) {
+			playerips.put(player, player.getAddress().getAddress());
+		}
 		Bukkit.getPluginManager().registerEvents(new EventListener(resourcemanager, positiontracker, playerips), this);
 		if (configoptions.hasplaceholderapi) {
 			new AMusicPlaceholderExpansion(positiontracker).register();
 		}
-		new AMusic(configoptions, positiontracker, resourcemanager, packsender, data);
 	}
 
 	public void onDisable() {

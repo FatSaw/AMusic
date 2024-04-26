72fbdf07ec7c57b443e0b166d503b49a66085899
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-04-26 07:27:42 +0300

    Update LoadmusicCommand.java

--- amusic_velocity/src/main/java/me/bomb/amusic/velocity/command/LoadmusicCommand.java
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/command/LoadmusicCommand.java
@@ -15,17 +15,17 @@ import me.bomb.amusic.PackSender;
 import me.bomb.amusic.PositionTracker;
 import me.bomb.amusic.ResourceFactory;
 import me.bomb.amusic.resourceserver.ResourceManager;
-import me.bomb.amusic.velocity.Data;
+import me.bomb.amusic.velocity.DataConfig;
 
 public class LoadmusicCommand implements SimpleCommand {
 	private final ProxyServer server;
 	private final ConfigOptions configoptions;
-	private final Data data;
+	private final DataConfig data;
 	private final ResourceManager resourcemanager;
 	private final PositionTracker positiontracker;
 	private final PackSender packsender;
 	
-	public LoadmusicCommand(ProxyServer server, ConfigOptions configoptions, Data data, ResourceManager resourcemanager, PositionTracker positiontracker, PackSender packsender) {
+	public LoadmusicCommand(ProxyServer server, ConfigOptions configoptions, DataConfig data, ResourceManager resourcemanager, PositionTracker positiontracker, PackSender packsender) {
 		this.server = server;
 		this.configoptions = configoptions;
 		this.data = data;

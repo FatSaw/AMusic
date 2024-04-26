a821a8aa82885377b21e22b07af9ce3202df4288
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-04-26 07:27:54 +0300

    Update VelocitySoundStarter.java

--- amusic_velocity/src/main/java/me/bomb/amusic/velocity/VelocitySoundStarter.java
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/VelocitySoundStarter.java
@@ -1,10 +1,7 @@
 package me.bomb.amusic.velocity;
 
 import java.util.Optional;
-import java.util.OptionalLong;
 import java.util.UUID;
-
-import org.jetbrains.annotations.NotNull;
 
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
@@ -12,7 +9,6 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import me.bomb.amusic.SoundStarter;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
-import net.kyori.adventure.sound.SoundStop;
 
 public class VelocitySoundStarter implements SoundStarter {
 	
@@ -29,59 +25,9 @@ public class VelocitySoundStarter implements SoundStarter {
 			return;
 		}
 		Player player = oplayer.get();
-		String soundname = "amusic.music".concat(Byte.toString(id));
-		Sound sound = new Sound() {
-			@Override
-			public float volume() {
-				return 1.0E9f;
-			}
-			
-			@Override
-			public @NotNull Source source() {
-				return Source.MASTER;
-			}
-			
-			@Override
-			public @NotNull OptionalLong seed() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public float pitch() {
-				return 1.0f;
-			}
-			
-			@Override
-			public @NotNull Key name() {
-				return new Key() {
-					
-					@Override
-					public @NotNull String value() {
-						return soundname;
-					}
-					
-					@Override
-					public @NotNull String namespace() {
-						// TODO Auto-generated method stub
-						return null;
-					}
-					
-					@Override
-					public @NotNull String asString() {
-						// TODO Auto-generated method stub
-						return soundname;
-					}
-				};
-			}
-			
-			@Override
-			public @NotNull SoundStop asStop() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-		};
-		player.playSound(sound);
+		Sound sound = Sound.sound(Key.key("amusic.music".concat(Byte.toString(id))), Sound.Source.MASTER, 1.0E9f, 1.0f);
+		player.playSound(sound); //Velocity playSound not implemented
+		
 	}
 
 }

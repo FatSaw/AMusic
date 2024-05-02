2a74f992f7534aa242a2326853680681ce142093
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-05-02 05:11:47 +0300

    Create ProtocoliseSoundStarter.java

---
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/ProtocoliseSoundStarter.java
@@ -0,0 +1,26 @@
+package me.bomb.amusic.velocity;
+
+import java.util.UUID;
+
+import dev.simplix.protocolize.api.Location;
+import dev.simplix.protocolize.api.Protocolize;
+import dev.simplix.protocolize.api.player.ProtocolizePlayer;
+import me.bomb.amusic.SoundStarter;
+
+public class ProtocoliseSoundStarter implements SoundStarter {
+	
+	protected ProtocoliseSoundStarter() {
+	}
+
+	@Override
+	public void startSound(UUID uuid, byte id) {
+		ProtocolizePlayer player = Protocolize.playerProvider().player(uuid);
+		if(player==null) {
+			return;
+		}
+		Location location = player.location();
+		NamedSoundEffectPacket customsound = new NamedSoundEffectPacket("amusic.music".concat(Byte.toString(id)), 0, location.x(), location.y(), location.z(), 1.0E9f, 1.0f);
+		player.sendPacket(customsound);
+	}
+
+}

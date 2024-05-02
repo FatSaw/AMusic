a6373ef7f114c5027455c6444dd4fd55cb60af6f
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-05-02 05:11:49 +0300

    Create ProtocoliseSoundStopper.java

---
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/ProtocoliseSoundStopper.java
@@ -0,0 +1,24 @@
+package me.bomb.amusic.velocity;
+
+import java.util.UUID;
+
+import dev.simplix.protocolize.api.Protocolize;
+import dev.simplix.protocolize.api.player.ProtocolizePlayer;
+import me.bomb.amusic.SoundStopper;
+
+public class ProtocoliseSoundStopper implements SoundStopper {
+	
+	protected ProtocoliseSoundStopper() {
+	}
+
+	@Override
+	public void stopSound(UUID uuid, byte id) {
+		ProtocolizePlayer player = Protocolize.playerProvider().player(uuid);
+		if(player==null) {
+			return;
+		}
+		SoundStopPacket soundstop = new SoundStopPacket("amusic.music".concat(Byte.toString(id)));
+		player.sendPacket(soundstop);
+	}
+
+}

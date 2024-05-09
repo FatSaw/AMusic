252f89c7f79a8f700a1db270858bd8db71973e58
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-05-09 05:07:08 +0300

    Update LangOptions.java

--- amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/command/LangOptions.java
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/command/LangOptions.java
@@ -1,6 +1,7 @@
 package me.bomb.amusic.bukkit.command;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,15 +9,13 @@ import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.bomb.amusic.SimpleConfiguration;
 import me.bomb.amusic.bukkit.AMusicBukkit;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
@@ -25,11 +24,11 @@ enum LangOptions {
 	loadmusic_usage, loadmusic_nopermission, loadmusic_nopermissionother, loadmusic_noconsoleselector, loadmusic_targetoffline, loadmusic_noplaylist, loadmusic_loaderunavilable, loadmusic_success, playmusic_usage, playmusic_nopermission, playmusic_nopermissionother, playmusic_noconsoleselector, playmusic_targetoffline, playmusic_noplaylist, playmusic_missingtrack, playmusic_success, playmusic_stop, repeat_usage, repeat_nopermission, repeat_nopermissionother, repeat_noconsoleselector, repeat_targetoffline, repeat_unknownrepeattype, repeat_repeatall, repeat_repeatone, repeat_playall, repeat_playone, repeat_random;
 	static {
 		JavaPlugin plugin = JavaPlugin.getPlugin(AMusicBukkit.class);
-		YamlConfiguration alang = null;
 		File langfile = new File(plugin.getDataFolder(), "lang.yml");
+		byte[] buf = null;
 		if (!langfile.exists()) {
 			try {
-				byte[] buf = new byte[8096];
+				buf = new byte[8096];
 				String nmsversion = Bukkit.getServer().getClass().getPackage().getName().substring(23);
 				InputStream in = plugin.getResource(nmsversion.equals("v1_7_R4") || nmsversion.equals("v1_8_R3") || nmsversion.equals("v1_9_R2") || nmsversion.equals("v1_10_R1") || nmsversion.equals("v1_11_R1") || nmsversion.equals("v1_12_R1") || nmsversion.equals("v1_13_R2") || nmsversion.equals("v1_14_R1") || nmsversion.equals("v1_15_R1") ? "lang_old.yml" : "lang_rgb.yml");
 				if (in != null) {
@@ -43,51 +42,40 @@ enum LangOptions {
 				}
 			} catch (IOException e) {
 			}
+		} else {
+			try {
+				FileInputStream in = new FileInputStream(langfile);
+				buf = new byte[in.available()];
+				in.read(buf);
+				in.close();
+			} catch (IOException e) {
+			}
 		}
-		alang = YamlConfiguration.loadConfiguration(langfile);
-		ConfigurationSection deffaultconfigurationsection = alang.getConfigurationSection("");
-		Set<String> aavilablelocales = null;
-
-		if (deffaultconfigurationsection != null) {
-			aavilablelocales = deffaultconfigurationsection.getKeys(false);
-			aavilablelocales.add("default");
+		SimpleConfiguration sc = new SimpleConfiguration(buf);
+		String[] locales = sc.getSubKeys("");
+		int i = locales.length;
+		while(--i > -1) {
+			String locale = locales[i];
 			for (LangOptions lang : values()) {
-				for (String locale : aavilablelocales) {
-					String optionpath = locale.concat(".").concat(lang.name().replace("_", "."));
-					String msg = alang.getString(optionpath);
-					if (!msg.isEmpty()) {
-						char startchar = msg.charAt(0), endchar = msg.charAt(msg.length() - 1);
-						if (startchar == '[' && endchar == ']' || startchar == '{' && endchar == '}') {
-							msg = TextComponent.toLegacyText(ComponentSerializer.parse(msg));
-						}
+				String optionpath = locale.concat(".").concat(lang.name().replace("_", "."));
+				String msg = sc.getStringOrDefault(optionpath, optionpath);
+				if (!msg.isEmpty()) {
+					char startchar = msg.charAt(0), endchar = msg.charAt(msg.length() - 1);
+					if (startchar == '[' && endchar == ']' || startchar == '{' && endchar == '}') {
+						msg = TextComponent.toLegacyText(ComponentSerializer.parse(msg));
 					}
-					lang.text.put(locale, msg);
 				}
+				lang.text.put(locale, msg);
 			}
 		}
-		avilablelocales = aavilablelocales;
 	}
-	private static final Set<String> avilablelocales;
 	private final Map<String, String> text = new HashMap<String, String>();
-	
-	public void sendMsgPlayer(Player target, Placeholders... placeholders) {
-		String locale = getLocale(target);
-		String msg = text.get(avilablelocales.contains(locale) ? locale : "default");
-		if (!msg.isEmpty()) {
-			for (Placeholders placeholder : placeholders) {
-				msg = msg.replace(placeholder.placeholder, placeholder.value);
-			}
-			target.sendMessage(msg);
-		}
-	}
 
 	public void sendMsg(CommandSender target, Placeholders... placeholders) {
 		String msg = null;
 		if (target instanceof Player) {
 			String locale = getLocale((Player) target);
-			if (avilablelocales.contains(locale)) {
-				msg = text.get(locale);
-			}
+			msg = text.get(locale);
 		}
 		if(msg==null) {
 			msg = text.get("default");

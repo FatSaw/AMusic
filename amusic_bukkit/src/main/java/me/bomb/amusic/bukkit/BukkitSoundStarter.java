dbf88319e9588d6ef3bb855aaa773de88e8c3795
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-05-11 17:25:23 +0300

    Update BukkitSoundStarter.java

--- amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/BukkitSoundStarter.java
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/BukkitSoundStarter.java
@@ -13,12 +13,12 @@ public final class BukkitSoundStarter implements SoundStarter {
 	}
 
 	@Override
-	public void startSound(UUID uuid, byte id) {
+	public void startSound(UUID uuid, short id) {
 		if(uuid == null) {
 			return;
 		}
 		Player player = Bukkit.getPlayer(uuid);
-		player.playSound(player.getLocation(), "amusic.music".concat(Byte.toString(id)), 1.0E9f, 1.0f);
+		player.playSound(player.getLocation(), "amusic.music".concat(Short.toString(id)), 1.0E9f, 1.0f);
 	}
 
 }

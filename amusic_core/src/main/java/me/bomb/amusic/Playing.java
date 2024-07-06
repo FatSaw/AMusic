214885e978ec0e99e114767ad6f8497188e9f58c
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-07-06 12:31:58 +0300

    Update Playing.java

--- amusic_core/src/main/java/me/bomb/amusic/Playing.java
+++ amusic_core/src/main/java/me/bomb/amusic/Playing.java
@@ -2,34 +2,11 @@ package me.bomb.amusic;
 
 public final class Playing {
 	protected final short currenttrack, maxid;
-	protected short remaining, remainingf;
+	protected short remaining;
 
-	protected Playing(short currenttrack, short maxid, short remaining, boolean processformatedtime) {
+	protected Playing(short currenttrack, short maxid, short remaining) {
 		this.currenttrack = currenttrack;
 		this.maxid = maxid;
 		this.remaining = remaining;
-		this.remainingf = processformatedtime ? sectotime(remaining) : 0;
-	}
-	
-	protected short sectotime(short sec) {
-		if (sec < 1)
-			return 0;
-		if (sec > 0x7080) {
-			return 0x7EFB;
-		}
-		short res = 0;
-		byte min = 0, hour = 0;
-		while ((res += 60) < sec) {
-			if (++min < 60) {
-				continue;
-			}
-			min = 0;
-			++hour;
-		}
-		res -= 59;
-		sec -= res;
-		sec |= ((short) min << 6);
-		sec |= ((short) hour << 6);
-		return sec;
 	}
 }

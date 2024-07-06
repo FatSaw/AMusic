2804059ca2312ffee0061f19099a092ffdc7ea4f
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-07-06 22:29:04 +0300

    Update NamedSoundEffectPacket.java

--- amusic_velocity/src/main/java/me/bomb/amusic/velocity/NamedSoundEffectPacket.java
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/NamedSoundEffectPacket.java
@@ -28,7 +28,8 @@ public class NamedSoundEffectPacket extends AbstractPacket {
         AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_19_3, MINECRAFT_1_19_3, 0x5E),
         AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_19_4, MINECRAFT_1_20_1, 0x62),
         AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_20_2, MINECRAFT_1_20_2, 0x64),
-        AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_20_3, MINECRAFT_LATEST, 0x66)
+        AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_20_3, MINECRAFT_1_20_5, 0x66),
+        AbstractProtocolMapping.rangedIdMapping(MINECRAFT_1_20_6, MINECRAFT_1_21, 0x68)
     );
     protected int soundid = 0;
     protected String sound;

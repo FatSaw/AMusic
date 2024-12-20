73adc73266d134b8f8017b2a1dd759bd6221ec87
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-20 03:32:35 +0200

    Create IChatBaseComponent.java

---
+++ amusic_bukkit/src/main/java/net/minecraft/server/v1_8_R3/IChatBaseComponent.java
@@ -0,0 +1,12 @@
+package net.minecraft.server.v1_8_R3;
+/**
+ * Fake nms
+ */
+public interface IChatBaseComponent {
+	public static final class ChatSerializer {
+		public static IChatBaseComponent a(String msg) {
+			return null;
+		}
+	}
+	public String c();
+}

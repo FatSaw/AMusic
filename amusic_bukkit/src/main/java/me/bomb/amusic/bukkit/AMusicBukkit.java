54e5d35d4fbb220bb2781b35be028f06fb5c07fa
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-20 03:32:03 +0200

    Update AMusicBukkit.java

--- amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/AMusicBukkit.java
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/AMusicBukkit.java
@@ -9,6 +9,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.bomb.amusic.AMusic;
@@ -25,6 +26,9 @@ import me.bomb.amusic.bukkit.command.RepeatCommand;
 import me.bomb.amusic.bukkit.command.RepeatTabComplete;
 import me.bomb.amusic.bukkit.command.SelectorProcessor;
 import me.bomb.amusic.bukkit.command.UploadmusicCommand;
+import me.bomb.amusic.bukkit.legacy.LegacyMessageSender_1_7_R4;
+import me.bomb.amusic.bukkit.legacy.LegacyMessageSender_1_8_R3;
+import me.bomb.amusic.bukkit.legacy.LegacyMessageSender_1_9_R2;
 import me.bomb.amusic.bukkit.legacy.LegacyPackSender_1_10_R1;
 import me.bomb.amusic.bukkit.legacy.LegacyPackSender_1_7_R4;
 import me.bomb.amusic.bukkit.legacy.LegacyPackSender_1_8_R3;
@@ -44,6 +48,7 @@ public final class AMusicBukkit extends JavaPlugin {
 	private final ResourceManager resourcemanager;
 	private final ConcurrentHashMap<Object,InetAddress> playerips;
 	private final PositionTracker positiontracker;
+	private final boolean waitacception;
 
 	public AMusicBukkit() {
 		byte ver = 127;
@@ -66,38 +71,45 @@ public final class AMusicBukkit extends JavaPlugin {
 		boolean waitacception = true;
 		PackSender packsender;
 		SoundStopper soundstopper;
+		MessageSender messagesender;
 		switch (ver) {
 		case 7:
 			packsender = new LegacyPackSender_1_7_R4();
 			soundstopper = new LegacySoundStopper_1_7_R4();
+			messagesender = new LegacyMessageSender_1_7_R4();
 			waitacception = false;
 		break;
 		case 8:
 			packsender = new LegacyPackSender_1_8_R3();
 			soundstopper = new LegacySoundStopper_1_8_R3();
+			messagesender = new LegacyMessageSender_1_8_R3();
 		break;
 		case 9:
 			packsender = new LegacyPackSender_1_9_R2();
 			soundstopper = new LegacySoundStopper_1_9_R2();
+			messagesender = new LegacyMessageSender_1_9_R2();
 		break;
 		case 10:
 			packsender = new LegacyPackSender_1_10_R1();
 			soundstopper = new BukkitSoundStopper();
+			messagesender = new SpigotMessageSender();
 		break;
 		default:
 			packsender = new BukkitPackSender();
 			soundstopper = new BukkitSoundStopper();
+			messagesender = new SpigotMessageSender();
 		break;
 		}
 		int maxpacksize = ver < 15 ? 52428800 : ver < 18 ? 104857600 : 262144000;
 		ConfigOptions configoptions = new ConfigOptions(configfile, maxpacksize, musicdir, packeddir, waitacception);
+		this.waitacception = configoptions.waitacception;
 		playerips = configoptions.resourcestrictaccess || configoptions.uploaderstrictaccess ? new ConcurrentHashMap<Object,InetAddress>(16,0.75f,1) : null;
 		Runtime runtime = Runtime.getRuntime();
 		SoundSource<?> source = configoptions.useconverter ? configoptions.encodetracksasynchronly ? new LocalUnconvertedParallelSource(runtime, configoptions.musicdir, configoptions.maxmusicfilesize, configoptions.ffmpegbinary, configoptions.bitrate, configoptions.channels, configoptions.samplingrate) : new LocalUnconvertedSource(runtime, configoptions.musicdir, configoptions.maxmusicfilesize, configoptions.ffmpegbinary, configoptions.bitrate, configoptions.channels, configoptions.samplingrate) : new LocalConvertedSource(configoptions.musicdir, configoptions.maxmusicfilesize);
 		this.amusic = new AMusic(configoptions, source, packsender, new BukkitSoundStarter(), soundstopper, playerips);
 		this.resourcemanager = amusic.resourcemanager;
 		this.positiontracker = amusic.positiontracker;
-		LangOptions.loadLang(langfile, ver > 15);
+		LangOptions.loadLang(messagesender, langfile, ver > 15);
 		amusic.setAPI();
 	}
 
@@ -119,14 +131,19 @@ public final class AMusicBukkit extends JavaPlugin {
 		repeatcommand.setExecutor(new RepeatCommand(server, positiontracker, selectorprocessor));
 		repeatcommand.setTabCompleter(new RepeatTabComplete(server));
 		PluginCommand uploadmusiccommand = getCommand("uploadmusic");
-		uploadmusiccommand.setExecutor(new UploadmusicCommand(amusic.uploadermanager));
+		UploadmusicCommand uploadmusiccmd = new UploadmusicCommand(amusic.uploadermanager);
+		uploadmusiccommand.setExecutor(uploadmusiccmd);
 		if(playerips != null) {
 			playerips.clear();
 			for(Player player : Bukkit.getOnlinePlayers()) {
 				playerips.put(player, player.getAddress().getAddress());
 			}
 		}
-		Bukkit.getPluginManager().registerEvents(new EventListener(resourcemanager, positiontracker, playerips), this);
+		PluginManager pluginmanager = server.getPluginManager();
+		pluginmanager.registerEvents(new EventListener(resourcemanager, positiontracker, playerips, uploadmusiccmd), this);
+		if(waitacception) {
+			pluginmanager.registerEvents(new PackStatusEventListener(resourcemanager, positiontracker), this);
+		}
 		this.amusic.enable();
 	}
 

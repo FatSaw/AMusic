b82e4cdbe9a95973c608f3420777a1e5e46f0ee0
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-20 03:32:32 +0200

    Create PacketPlayOutChat.java

---
+++ amusic_bukkit/src/main/java/net/minecraft/server/v1_7_R4/PacketPlayOutChat.java
@@ -0,0 +1,9 @@
+package net.minecraft.server.v1_7_R4;
+/**
+ * Fake nms
+ */
+public final class PacketPlayOutChat implements Packet<PacketPlayOutChat> {
+	public PacketPlayOutChat(IChatBaseComponent icbc, boolean b) {
+		
+	}
+}

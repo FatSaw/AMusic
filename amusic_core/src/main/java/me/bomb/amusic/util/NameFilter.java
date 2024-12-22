490f9d717a1a832e2387135436150a800c0d9b31
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-22 17:03:56 +0200

    Create NameFilter.java

---
+++ amusic_core/src/main/java/me/bomb/amusic/util/NameFilter.java
@@ -0,0 +1,32 @@
+package me.bomb.amusic.util;
+
+import static java.io.File.separatorChar;
+
+public final class NameFilter {
+	
+	public static String filterName(String name) {
+		char[] chars = name.toCharArray();
+		int finalcount = 0;
+		int i = chars.length;
+		while(--i > -1) {
+			char c = chars[i];
+			//if(c == separatorChar || c == '/' || c == '\\' || c == ':' || c == '<' || c == '>' || c == '*' || c == '?' || c == '|' || c == '\"' || (c > 0 && c < 32)) {
+			if(c == separatorChar || c == '/' || c == '\\') {
+				chars[i] = '\0';
+			} else {
+				++finalcount;
+			}
+		}
+		char[] filtered = new char[finalcount];
+		int j = 0;
+		while(++i < chars.length && j < finalcount) {
+			char c = chars[i];
+			if(c != '\0') {
+				filtered[j] = c;
+				++j;
+			}
+		}
+		return new String(filtered);
+	}
+
+}

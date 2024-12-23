05ea7232c6fdd431847756e50cb11fa435c5e039
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-23 15:26:50 +0200

    Update ProtocoliseSoundStarter.java

--- amusic_velocity/src/main/java/me/bomb/amusic/velocity/ProtocoliseSoundStarter.java
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/ProtocoliseSoundStarter.java
@@ -2,11 +2,8 @@ package me.bomb.amusic.velocity;
 
 import java.util.UUID;
 
-import dev.simplix.protocolize.api.Location;
 import dev.simplix.protocolize.api.Protocolize;
-import dev.simplix.protocolize.api.SoundCategory;
 import dev.simplix.protocolize.api.player.ProtocolizePlayer;
-import dev.simplix.protocolize.data.packets.NamedSoundEffect;
 import me.bomb.amusic.SoundStarter;
 
 public final class ProtocoliseSoundStarter implements SoundStarter {
@@ -20,9 +17,7 @@ public final class ProtocoliseSoundStarter implements SoundStarter {
 		if(player==null) {
 			return;
 		}
-		Location location = player.location();
-		NamedSoundEffect customsound = new NamedSoundEffect(player.protocolVersion(), "amusic.music".concat(Short.toString(id)), SoundCategory.MASTER, location.x(), location.y(), location.z(), 1.0E9f, 1.0f, 0);
-		//NamedSoundEffectPacket customsound = new NamedSoundEffectPacket("amusic.music".concat(Short.toString(id)), 0, location.x(), location.y(), location.z(), 1.0E9f, 1.0f);
+		NamedSoundEffectPacket customsound = new NamedSoundEffectPacket("amusic.music".concat(Short.toString(id)), 0, 0, Integer.MIN_VALUE, 0, 1.0E9f, 1.0f);
 		player.sendPacket(customsound);
 	}
 

baeb6accfe68f23080a8e36c2b11d0f88f789ab2
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-24 20:45:31 +0200

    Update SpigotMessageSender.java

--- amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/SpigotMessageSender.java
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/SpigotMessageSender.java
@@ -1,16 +1,36 @@
 package me.bomb.amusic.bukkit;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import me.bomb.amusic.MessageSender;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 
-public class SpigotMessageSender implements MessageSender {
+public final class SpigotMessageSender implements MessageSender {
+	
+	protected SpigotMessageSender() {
+	}
 
 	@Override
-	public void send(CommandSender target, String message) {
-		BaseComponent[] bcmessage = ComponentSerializer.parse(message);
-		target.spigot().sendMessage(bcmessage);
+	public final void send(Object target, String message) {
+		if(target instanceof CommandSender) {
+			BaseComponent[] bcmessage = ComponentSerializer.parse(message);
+			((CommandSender)target).spigot().sendMessage(bcmessage);
+		}
+	}
+	
+	@Override
+	public final String getLocale(Object target) {
+		if(target instanceof Player) {
+			Player player = (Player)target;
+			try {
+				return player.getLocale().toLowerCase();
+			} catch (NoSuchMethodError e) {
+				return player.spigot().getLocale().toLowerCase();
+			}
+		}
+		return null;
 	}
 
 }

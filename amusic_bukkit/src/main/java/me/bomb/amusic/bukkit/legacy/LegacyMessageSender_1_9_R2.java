9ae9cf79e777ea787e9a36d3deed544211082f11
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-24 20:45:27 +0200

    Update LegacyMessageSender_1_9_R2.java

--- amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/legacy/LegacyMessageSender_1_9_R2.java
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/legacy/LegacyMessageSender_1_9_R2.java
@@ -2,9 +2,8 @@ package me.bomb.amusic.bukkit.legacy;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
-import org.bukkit.entity.Player;
 
-import me.bomb.amusic.bukkit.MessageSender;
+import me.bomb.amusic.MessageSender;
 import net.minecraft.server.v1_9_R2.IChatBaseComponent;
 import net.minecraft.server.v1_9_R2.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_9_R2.PacketPlayOutChat;
@@ -13,14 +12,27 @@ import net.minecraft.server.v1_9_R2.PlayerConnection;
 public final class LegacyMessageSender_1_9_R2 implements MessageSender {
 
 	@Override
-	public void send(CommandSender target, String message) {
+	public void send(Object target, String message) {
 		IChatBaseComponent chatbasecomponent = ChatSerializer.a(message);
-		if(target instanceof Player) {
+		if(target instanceof CraftPlayer) {
 			PlayerConnection playerconnection = ((CraftPlayer)target).getHandle().playerConnection;
 			playerconnection.sendPacket(new PacketPlayOutChat(chatbasecomponent,(byte) 0));
 			return;
 		}
-		target.sendMessage(chatbasecomponent.toPlainText());
+		((CommandSender)target).sendMessage(chatbasecomponent.toPlainText());
+	}
+	
+	@Override
+	public final String getLocale(Object target) {
+		if(target instanceof CraftPlayer) {
+			CraftPlayer player = (CraftPlayer)target;
+			try {
+				return player.getLocale().toLowerCase();
+			} catch (NoSuchMethodError e) {
+				return player.spigot().getLocale().toLowerCase();
+			}
+		}
+		return null;
 	}
 
 }

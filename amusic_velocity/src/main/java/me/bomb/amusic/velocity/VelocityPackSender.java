9dd7ad41bc3b3762a6bbe0847ade37a69605597f
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-25 16:00:33 +0200

    Update VelocityPackSender.java

--- amusic_velocity/src/main/java/me/bomb/amusic/velocity/VelocityPackSender.java
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/VelocityPackSender.java
@@ -1,19 +1,24 @@
 package me.bomb.amusic.velocity;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.UUID;
 
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.messages.ChannelIdentifier;
+import com.velocitypowered.api.proxy.messages.LegacyChannelIdentifier;
 
 import me.bomb.amusic.PackSender;
 
 public final class VelocityPackSender implements PackSender {
 
 	private final ProxyServer server;
+	private final ChannelIdentifier identifier;
 	
 	protected VelocityPackSender(ProxyServer server) {
 		this.server = server;
+		this.identifier = new LegacyChannelIdentifier("MC|RPack");
 	}
 	
 	@Override
@@ -23,7 +28,16 @@ public final class VelocityPackSender implements PackSender {
 			return;
 		}
 		Player player = oplayer.get();
-		
+		if (player.getProtocolVersion().getProtocol() < 6) {
+			byte[] urlb = url.getBytes(StandardCharsets.UTF_8), buf = new byte[2 + urlb.length];
+			short length = (short) urlb.length;
+			System.arraycopy(urlb, 0, buf, 2, length);
+			urlb[0] = (byte) length;
+			length >>= 8;
+			urlb[1] = (byte) length;
+			player.sendPluginMessage(identifier, buf);
+			return;
+		}
 		try {
 			player.sendResourcePack(url, sha1);
 		} catch (IllegalStateException e) {

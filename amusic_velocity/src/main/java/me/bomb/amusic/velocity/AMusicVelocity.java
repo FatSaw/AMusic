c0a22877c6219aeff637a0aa2e8281a95da68790
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-31 03:05:26 +0200

    Update AMusicVelocity.java

--- amusic_velocity/src/main/java/me/bomb/amusic/velocity/AMusicVelocity.java
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/AMusicVelocity.java
@@ -24,6 +24,7 @@ import dev.simplix.protocolize.api.Protocolize;
 import dev.simplix.protocolize.api.providers.ProtocolRegistrationProvider;
 import me.bomb.amusic.AMusic;
 import me.bomb.amusic.ConfigOptions;
+import me.bomb.amusic.LocalAMusic;
 import me.bomb.amusic.PackSender;
 import me.bomb.amusic.PositionTracker;
 import me.bomb.amusic.resourceserver.ResourceManager;
@@ -37,7 +38,7 @@ import me.bomb.amusic.velocity.command.PlaymusicCommand;
 import me.bomb.amusic.velocity.command.RepeatCommand;
 import me.bomb.amusic.velocity.command.UploadmusicCommand;
 
-@Plugin(id = "amusic", name = "AMusic", dependencies = {@Dependency(id = "protocolize")}, version = "0.15", authors = {"Bomb"})
+@Plugin(id = "amusic", name = "AMusic", dependencies = {@Dependency(id = "protocolize")}, version = "0.16", authors = {"Bomb"})
 public final class AMusicVelocity {
 	
 	private final ProxyServer server;
@@ -46,7 +47,7 @@ public final class AMusicVelocity {
 	private final ResourceManager resourcemanager;
 	private final ConcurrentHashMap<Object,InetAddress> playerips;
 	private final PositionTracker positiontracker;
-	private final String configerrors;
+	private final String configerrors, uploaderhost;
     
 	@Inject
 	public AMusicVelocity(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
@@ -66,19 +67,29 @@ public final class AMusicVelocity {
 		int maxpacksize = 262144000;
 		ConfigOptions configoptions = new ConfigOptions(configfile, maxpacksize, musicdir, packeddir, true);
 		this.configerrors = configoptions.loaderrors;
+		this.uploaderhost = configoptions.useuploader ? configoptions.uploaderhost : null;
 		playerips = configoptions.resourcestrictaccess || configoptions.uploaderstrictaccess ? new ConcurrentHashMap<Object,InetAddress>(16,0.75f,1) : null;
 
 		PackSender packsender = new VelocityPackSender(server);
         
 		Runtime runtime = Runtime.getRuntime();
 		SoundSource<?> source = configoptions.useconverter ? configoptions.encodetracksasynchronly ? new LocalUnconvertedParallelSource(runtime, configoptions.musicdir, configoptions.maxmusicfilesize, configoptions.ffmpegbinary, configoptions.bitrate, configoptions.channels, configoptions.samplingrate) : new LocalUnconvertedSource(runtime, configoptions.musicdir, configoptions.maxmusicfilesize, configoptions.ffmpegbinary, configoptions.bitrate, configoptions.channels, configoptions.samplingrate) : new LocalConvertedSource(configoptions.musicdir, configoptions.maxmusicfilesize);
-		this.amusic = new AMusic(configoptions, source, packsender, new ProtocoliseSoundStarter(), new ProtocoliseSoundStopper(server, true), playerips);
-		this.resourcemanager = amusic.resourcemanager;
-		this.positiontracker = amusic.positiontracker;
+		final boolean localapi = true;
+		if(localapi) {
+			LocalAMusic amusic =  new LocalAMusic(configoptions, source, packsender, new ProtocoliseSoundStarter(), new ProtocoliseSoundStopper(server, true), playerips);
+			this.resourcemanager = amusic.resourcemanager;
+			this.positiontracker = amusic.positiontracker;
+			amusic.setAPI();
+			this.amusic = amusic;
+		} else {
+			this.resourcemanager = null;
+			this.positiontracker = null;
+		}
+		
 		this.server = server;
         this.logger = logger;
 		LangOptions.loadLang(new VelocityMessageSender(), langfile, false);
-        amusic.setAPI();
+        
     }
 	
 	@Subscribe
@@ -90,10 +101,10 @@ public final class AMusicVelocity {
 		ProtocolRegistrationProvider protocolregistration = Protocolize.protocolRegistration();
 		protocolregistration.registerPacket(SoundStopPacket.MAPPINGS, Protocol.PLAY, PacketDirection.CLIENTBOUND, SoundStopPacket.class);
 		protocolregistration.registerPacket(NamedSoundEffectPacket.MAPPINGS, Protocol.PLAY, PacketDirection.CLIENTBOUND, NamedSoundEffectPacket.class);
-		LoadmusicCommand loadmusic = new LoadmusicCommand(server, amusic.source, amusic.datamanager, amusic.dispatcher);
-		PlaymusicCommand playmusic = new PlaymusicCommand(server, positiontracker, true), playmusicuntrackable = new PlaymusicCommand(server, positiontracker, false);
-		RepeatCommand repeat = new RepeatCommand(server, positiontracker);
-		UploadmusicCommand uploadmusic = new UploadmusicCommand(amusic.uploadermanager);
+		LoadmusicCommand loadmusic = new LoadmusicCommand(server, amusic);
+		PlaymusicCommand playmusic = new PlaymusicCommand(server, amusic, true), playmusicuntrackable = new PlaymusicCommand(server, amusic, false);
+		RepeatCommand repeat = new RepeatCommand(server, amusic);
+		UploadmusicCommand uploadmusic = new UploadmusicCommand(amusic, uploaderhost);
 		CommandManager cmdmanager = this.server.getCommandManager();
 		CommandMeta loadmusicmeta = cmdmanager.metaBuilder("loadmusic").plugin(this).build(), playmusicmeta = cmdmanager.metaBuilder("playmusic").plugin(this).build(), playmusicuntrackablemeta = cmdmanager.metaBuilder("playmusicuntrackable").plugin(this).build(), repeatmeta = cmdmanager.metaBuilder("repeat").plugin(this).build(), uploadmusicmeta = cmdmanager.metaBuilder("uploadmusic").plugin(this).build();
 		cmdmanager.register(loadmusicmeta, loadmusic);
@@ -101,7 +112,9 @@ public final class AMusicVelocity {
 		cmdmanager.register(playmusicuntrackablemeta, playmusicuntrackable);
 		cmdmanager.register(repeatmeta, repeat);
 		cmdmanager.register(uploadmusicmeta, uploadmusic);
-		this.server.getEventManager().register(this, new EventListener(resourcemanager, positiontracker, playerips, uploadmusic));
+		if(this.resourcemanager != null) {
+			this.server.getEventManager().register(this, new EventListener(resourcemanager, positiontracker, playerips, uploadmusic));
+		}
 		this.amusic.enable();
 	}
 	

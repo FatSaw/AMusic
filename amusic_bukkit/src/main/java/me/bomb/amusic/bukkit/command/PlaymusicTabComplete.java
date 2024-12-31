bcc9915943faeee7597178668c39292fba791a92
FatSaw <100958165+FatSaw@users.noreply.github.com>
2024-12-31 03:05:37 +0200

    Update PlaymusicTabComplete.java

--- amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/command/PlaymusicTabComplete.java
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/command/PlaymusicTabComplete.java
@@ -9,15 +9,14 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-import me.bomb.amusic.PositionTracker;
-import me.bomb.amusic.packedinfo.SoundInfo;
+import me.bomb.amusic.AMusic;
 
 public final class PlaymusicTabComplete implements TabCompleter {
 	private final Server server;
-	private final PositionTracker positiontracker;
-	public PlaymusicTabComplete(Server server, PositionTracker positiontracker) {
+	private final AMusic amusic;
+	public PlaymusicTabComplete(Server server, AMusic amusic) {
 		this.server = server;
-		this.positiontracker = positiontracker;
+		this.amusic = amusic;
 	}
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
@@ -48,8 +47,8 @@ public final class PlaymusicTabComplete implements TabCompleter {
 			if (selfsender || !(sender instanceof Player) || sender.hasPermission("amusic.playmusic.other")) {
 				Player target = server.getPlayerExact(args[0]);
 				if (target != null) {
-					SoundInfo[] soundsinfo = positiontracker.getSoundInfo(target.getUniqueId());
-					if (soundsinfo != null) {
+					List<String> soundnames = amusic.getPlaylistSoundnames(target.getUniqueId());
+					if (soundnames != null) {
 						int lastspace = -1;
 						if(args.length > 2) {
 							StringBuilder sb = new StringBuilder(args[1]);
@@ -63,15 +62,13 @@ public final class PlaymusicTabComplete implements TabCompleter {
 						++lastspace;
 						
 						if(lastspace == 0) {
-							for (SoundInfo soundinfo : soundsinfo) {
-								String soundname = soundinfo.name;
+							for (String soundname : soundnames) {
 								if (soundname.startsWith(args[1])) {
 									tabcomplete.add(soundname);
 								}
 							}
 						} else {
-							for (SoundInfo soundinfo : soundsinfo) {
-								String soundname = soundinfo.name;
+							for (String soundname : soundnames) {
 								if (lastspace < soundname.length() && soundname.startsWith(args[1])) {
 									soundname = soundname.substring(lastspace);
 									tabcomplete.add(soundname);

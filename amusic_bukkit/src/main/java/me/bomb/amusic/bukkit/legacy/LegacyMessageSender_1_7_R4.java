6b0f041d8d11c89ce976be9bc46359e377bdaaa2
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-12 05:20:50 +0200

    Update LegacyMessageSender_1_7_R4.java

--- amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/legacy/LegacyMessageSender_1_7_R4.java
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/legacy/LegacyMessageSender_1_7_R4.java
@@ -26,11 +26,7 @@ public final class LegacyMessageSender_1_7_R4 implements MessageSender {
 	public final String getLocale(Object target) {
 		if(target instanceof CraftPlayer) {
 			CraftPlayer player = (CraftPlayer)target;
-			try {
-				return player.getLocale().toLowerCase();
-			} catch (NoSuchMethodError e) {
-				return player.spigot().getLocale().toLowerCase();
-			}
+			return player.getHandle().locale;
 		}
 		return null;
 	}

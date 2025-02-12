ac584e08cb383627c9c3ef50c63e0f44e33273cf
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-12 08:04:27 +0200

    Create PacketPlayOutCustomPayload.java

---
+++ amusic_bukkit/src/main/java/net/minecraft/server/v1_10_R1/PacketPlayOutCustomPayload.java
@@ -0,0 +1,9 @@
+package net.minecraft.server.v1_10_R1;
+
+/**
+ * Fake nms
+ */
+public class PacketPlayOutCustomPayload implements Packet<PacketPlayOutCustomPayload> {
+	public PacketPlayOutCustomPayload(String s, PacketDataSerializer packetdataserializer) {
+	}
+}

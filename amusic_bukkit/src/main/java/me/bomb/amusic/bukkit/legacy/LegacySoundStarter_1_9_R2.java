da2bbafabdf85949236665675a3bcdbb2bc4120c
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-12 05:21:00 +0200

    Create LegacySoundStarter_1_9_R2.java

---
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/legacy/LegacySoundStarter_1_9_R2.java
@@ -0,0 +1,23 @@
+package me.bomb.amusic.bukkit.legacy;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+
+import me.bomb.amusic.SoundStarter;
+import net.minecraft.server.v1_9_R2.PacketPlayOutCustomSoundEffect;
+import net.minecraft.server.v1_9_R2.SoundCategory;
+
+public class LegacySoundStarter_1_9_R2 implements SoundStarter {
+	
+	@Override
+	public void startSound(UUID uuid, short id) {
+		if(uuid == null) {
+			return;
+		}
+		CraftPlayer player = (CraftPlayer) Bukkit.getPlayer(uuid);
+		player.getHandle().playerConnection.sendPacket(new PacketPlayOutCustomSoundEffect("amusic.music".concat(Short.toString(id)), SoundCategory.VOICE, 0.0d, 0.0d, 0.0d, 1.0E9f, 1.0f));
+	}
+
+}

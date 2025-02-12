8fcad9362c9b5dc41c7447ec80f151ce7f1d661c
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-12 08:04:33 +0200

    Create LegacySoundStopper_1_10_R1.java

---
+++ amusic_bukkit/src/main/java/me/bomb/amusic/bukkit/legacy/LegacySoundStopper_1_10_R1.java
@@ -0,0 +1,33 @@
+package me.bomb.amusic.bukkit.legacy;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import io.netty.buffer.Unpooled;
+import me.bomb.amusic.SoundStopper;
+import net.minecraft.server.v1_10_R1.EntityPlayer;
+import net.minecraft.server.v1_10_R1.PacketDataSerializer;
+import net.minecraft.server.v1_10_R1.PlayerConnection;
+import net.minecraft.server.v1_10_R1.PacketPlayOutCustomPayload;
+
+public class LegacySoundStopper_1_10_R1 implements SoundStopper {
+
+	@Override
+	public void stopSound(UUID uuid, short id) {
+		if(uuid == null) {
+			return;
+		}
+		Player player = Bukkit.getPlayer(uuid);
+		EntityPlayer entityplayer = ((CraftPlayer)player).getHandle();
+		PlayerConnection connection = entityplayer.playerConnection;
+		PacketDataSerializer packetdataserializer = new PacketDataSerializer(Unpooled.buffer());
+        packetdataserializer.a("");
+        packetdataserializer.a("amusic.music".concat(Short.toString(id)));
+        connection.sendPacket(new PacketPlayOutCustomPayload("MC|StopSound", packetdataserializer));
+		
+	}
+
+}

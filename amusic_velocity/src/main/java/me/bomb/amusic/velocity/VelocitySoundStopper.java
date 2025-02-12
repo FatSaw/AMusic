878b0b831c3eaf949d4d0e17c83170a56ee9875a
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-12 06:44:06 +0200

    Update VelocitySoundStopper.java

--- amusic_velocity/src/main/java/me/bomb/amusic/velocity/VelocitySoundStopper.java
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/VelocitySoundStopper.java
@@ -26,7 +26,7 @@ public final class VelocitySoundStopper implements SoundStopper {
 		}
 		Player player = oplayer.get();
 		final int version = player.getProtocolVersion().getProtocol();
-		Sound sound = Sound.sound(Key.key("amusic.music".concat(Short.toString(id))), version < 107 ? Sound.Source.MASTER : Sound.Source.VOICE, 1.0E9f, 1.0f);
+		Sound sound = Sound.sound(Key.key("amusic.music".concat(Short.toString(id))), Sound.Source.VOICE, version < 393 ? 1.0E9f : 1.0f, 1.0f);
 		player.stopSound(sound); //Velocity stopSound not implemented
 		
 	}

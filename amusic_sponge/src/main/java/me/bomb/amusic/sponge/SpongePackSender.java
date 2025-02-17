2208ba8bb7a0523703e5fb9c3353fc59407ffa35
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-17 20:00:14 +0200

    Update SpongePackSender.java

--- amusic_sponge/src/main/java/me/bomb/amusic/sponge/SpongePackSender.java
+++ amusic_sponge/src/main/java/me/bomb/amusic/sponge/SpongePackSender.java
@@ -1,13 +1,13 @@
 package me.bomb.amusic.sponge;
 
-import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.spongepowered.api.Server;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.resourcepack.ResourcePack;
+import org.spongepowered.common.resourcepack.SpongeURIResourcePack;
 
 import me.bomb.amusic.PackSender;
 
@@ -27,7 +27,16 @@ public final class SpongePackSender implements PackSender {
 		Optional<Player> oplayer = server.getPlayer(uuid);
 		if(oplayer.isPresent()) {
 			Player player = oplayer.get();
-			player.sendResourcePack(new ResourcePack() {
+			SpongeURIResourcePack rp;
+			try {
+				rp = new SpongeURIResourcePack(url, bytesToHex(sha1));
+			} catch (URISyntaxException e) {
+				return;
+			}
+			player.sendResourcePack(rp);
+			/*player.sendResourcePack(new ResourcePack() {
+				
+				final String name = url.substring(url.lastIndexOf("/") + 1).replaceAll("\\W", "");
 				
 				@Override
 				public URI getUri() {
@@ -36,7 +45,7 @@ public final class SpongePackSender implements PackSender {
 				
 				@Override
 				public String getName() {
-					return uuid.toString();
+					return name;
 				}
 				
 				@Override
@@ -48,10 +57,9 @@ public final class SpongePackSender implements PackSender {
 				public Optional<String> getHash() {
 					return Optional.of(bytesToHex(sha1));
 				}
-			});
+			});*/
 		}
 	}
-	
 	private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
 	private static String bytesToHex(byte[] bytes) {
 		int i = bytes.length, j = i << 1;
@@ -63,5 +71,5 @@ public final class SpongePackSender implements PackSender {
 	    }
 	    return new String(hexChars, StandardCharsets.US_ASCII);
 	}
-
+	
 }

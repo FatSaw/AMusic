ded56a7a00aa24208f8e2dc680e92051eb570de6
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-17 06:43:13 +0200

    Create GlowstonePackSender.java

---
+++ amusic_glowstone/src/main/java/me/bomb/amusic/glowstone/GlowstonePackSender.java
@@ -0,0 +1,37 @@
+package me.bomb.amusic.glowstone;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import me.bomb.amusic.PackSender;
+
+public final class GlowstonePackSender implements PackSender {
+	
+	protected GlowstonePackSender() {
+	}
+
+	@Override
+	public void send(UUID uuid, String url, byte[] sha1) {
+		if(uuid == null) {
+			return;
+		}
+		Player player = Bukkit.getPlayer(uuid);
+		player.setResourcePack(url, bytesToHex(sha1));
+	}
+	
+	private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+	private static String bytesToHex(byte[] bytes) {
+		int i = bytes.length, j = i << 1;
+	    byte[] hexChars = new byte[j];
+	    while(--i > -1) {
+	    	int v = bytes[i] & 0xFF;
+	        hexChars[--j] = HEX_ARRAY[v & 0x0F];
+	    	hexChars[--j] = HEX_ARRAY[v >>> 4];
+	    }
+	    return new String(hexChars, StandardCharsets.US_ASCII);
+	}
+
+}

543ac5d2756257289aaea289b897adeb544d3174
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-17 10:01:15 +0200

    Create SpongeSoundStopper.java

---
+++ amusic_sponge/src/main/java/me/bomb/amusic/sponge/SpongeSoundStopper.java
@@ -0,0 +1,45 @@
+package me.bomb.amusic.sponge;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.spongepowered.api.Server;
+import org.spongepowered.api.effect.sound.SoundCategories;
+import org.spongepowered.api.effect.sound.SoundType;
+import org.spongepowered.api.entity.living.player.Player;
+
+
+import me.bomb.amusic.SoundStopper;
+
+public final class SpongeSoundStopper implements SoundStopper {
+	
+	private final Server server;
+	
+	protected SpongeSoundStopper(Server server) {
+		this.server = server;
+	}
+
+	@Override
+	public void stopSound(UUID uuid, short id) {
+		if(uuid == null) {
+			return;
+		}
+		Optional<Player> oplayer = server.getPlayer(uuid);
+		if(oplayer.isPresent()) {
+			Player player = oplayer.get();
+			SoundType sound = new SoundType() {
+				@Override
+				public String getName() {
+					return "amusic.music".concat(Short.toString(id));
+				}
+				@Override
+				public String getId() {
+					return "amusic.music".concat(Short.toString(id));
+				}
+			};
+			player.stopSounds(sound, SoundCategories.VOICE);
+		}
+		
+	}
+
+}

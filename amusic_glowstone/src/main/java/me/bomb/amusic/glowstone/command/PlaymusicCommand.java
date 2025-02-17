4a7e5d60754c1416076aeeecfc44a6326c448b71
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-17 06:42:53 +0200

    Create PlaymusicCommand.java

---
+++ amusic_glowstone/src/main/java/me/bomb/amusic/glowstone/command/PlaymusicCommand.java
@@ -0,0 +1,237 @@
+package me.bomb.amusic.glowstone.command;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.RemoteConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import me.bomb.amusic.AMusic;
+import me.bomb.amusic.util.LangOptions;
+import me.bomb.amusic.util.LangOptions.Placeholder;
+
+public final class PlaymusicCommand implements CommandExecutor {
+	private final Server server;
+	private final AMusic amusic;
+	private final SelectorProcessor selectorprocessor;
+	private final boolean trackable;
+	public PlaymusicCommand(Server server, AMusic amusic, SelectorProcessor selectorprocessor, boolean trackable) {
+		this.server = server;
+		this.amusic = amusic;
+		this.selectorprocessor = selectorprocessor;
+		this.trackable = trackable;
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(!sender.hasPermission("amusic.playmusic")) {
+			LangOptions.playmusic_nopermission.sendMsg(sender);
+			return true;
+		}
+		if(args.length==1) {
+			if(args[0].equals("@s")) {
+				if(sender instanceof Player) {
+					args[0] = sender.getName();
+				} else {
+					LangOptions.playmusic_noconsoleselector.sendMsg(sender);
+					return true;
+				}
+			} else if(!sender.hasPermission("amusic.playmusic.other")) {
+				LangOptions.playmusic_nopermissionother.sendMsg(sender);
+				return true;
+			} else {
+				if (args[0].startsWith("@p")) {
+					String closestplayername = selectorprocessor.getNearest(sender, args[0].substring(2));
+					
+					if(closestplayername == null) {
+						LangOptions.playmusic_unavilableselector_near.sendMsg(sender);
+						return true;
+					}
+					args[0] = closestplayername;
+				}
+				
+				if (args[0].startsWith("@r")) {
+					String randomplayername = selectorprocessor.getRandom(sender, args[0].substring(2));
+					if(randomplayername == null) {
+						LangOptions.playmusic_unavilableselector_random.sendMsg(sender);
+						return true;
+					}
+					args[0] = randomplayername;
+				}
+				if(args[0].startsWith("@a")) {
+					UUID[] targetarray = args[0].length() == 2 ? selectorprocessor.getAllGlobal() : selectorprocessor.getSameWorld(sender, args[0].substring(2)); 
+					if(targetarray == null) {
+						LangOptions.playmusic_unavilableselector_all.sendMsg(sender);
+						return true;
+					}
+					if(trackable) {
+						for(int i = targetarray.length; --i > -1;) {
+							amusic.stopSound(targetarray[i]);
+						}
+					} else {
+						for(int i = targetarray.length; --i > -1;) {
+							amusic.stopSoundUntrackable(targetarray[i]);
+						}
+					}
+					LangOptions.playmusic_stop.sendMsg(sender);
+					return true;
+				}
+				
+			}
+			
+			Player target = server.getPlayerExact(args[0]);
+			if(target==null) {
+				LangOptions.playmusic_targetoffline.sendMsg(sender);
+				return true;
+			}
+			if(trackable) {
+				amusic.stopSound(target.getUniqueId());
+			} else {
+				amusic.stopSoundUntrackable(target.getUniqueId());
+			}
+			
+			LangOptions.playmusic_stop.sendMsg(sender);
+		} else if(args.length>1) {
+			if(args[0].equals("@s")) {
+				if(sender instanceof Player) {
+					args[0] = sender.getName();
+				} else {
+					LangOptions.playmusic_noconsoleselector.sendMsg(sender);
+					return true;
+				}
+			} else if(args[0].equals("@l") && (sender instanceof ConsoleCommandSender || sender instanceof RemoteConsoleCommandSender)) {
+				Player target = server.getPlayerExact(args[1]);
+				if(target==null) {
+					LangOptions.playmusic_targetoffline.sendMsg(sender);
+					return true;
+				}
+				UUID targetuuid = target.getUniqueId();
+				String[] soundnames = amusic.getPlaylistSoundnames(targetuuid);
+				if(soundnames==null) {
+					LangOptions.playmusic_noplaylist.sendMsg(sender);
+					return true;
+				}
+				
+				String playing = amusic.getPlayingSoundName(targetuuid);
+				short playingsize = amusic.getPlayingSoundSize(targetuuid), playingstate = amusic.getPlayingSoundRemain(targetuuid);;
+				
+				StringBuilder sb = new StringBuilder();
+				if(playing!=null) {
+					sb.append("Playing: ");
+					sb.append(playing);
+					sb.append(' ');
+				}
+				if(playingsize!=-1&&playingstate!=-1) {
+					playingstate=(short) (playingsize-playingstate);
+					sb.append(Short.toString(playingstate));
+					sb.append('/');
+					sb.append(Short.toString(playingsize));
+					sb.append(' ');
+				}
+				sb.append("Sounds: ");
+				for(String soundname : soundnames) {
+					sb.append(soundname);
+					sb.append(' ');
+				}
+				sender.sendMessage(sb.toString());
+				return true;
+			} else if(!sender.hasPermission("amusic.playmusic.other")) {
+				LangOptions.playmusic_nopermissionother.sendMsg(sender);
+				return true;
+			} else {
+				if (args[0].startsWith("@p")) {
+					String closestplayername = selectorprocessor.getNearest(sender, args[0].substring(2));
+					
+					if(closestplayername == null) {
+						LangOptions.playmusic_unavilableselector_near.sendMsg(sender);
+						return true;
+					}
+					args[0] = closestplayername;
+				}
+				
+				if (args[0].startsWith("@r")) {
+					String randomplayername = selectorprocessor.getRandom(sender, args[0].substring(2));
+					if(randomplayername == null) {
+						LangOptions.playmusic_unavilableselector_random.sendMsg(sender);
+						return true;
+					}
+					args[0] = randomplayername;
+				}
+				if(args[0].startsWith("@a")) {
+					UUID[] targetarray = args[0].length() == 2 ? selectorprocessor.getAllGlobal() : selectorprocessor.getSameWorld(sender, args[0].substring(2)); 
+					if(targetarray == null) {
+						LangOptions.playmusic_unavilableselector_all.sendMsg(sender);
+						return true;
+					}
+					if(args.length>2) {
+						StringBuilder sb = new StringBuilder(args[1]);
+						for(int i = 2;i < args.length;++i) {
+							sb.append(' ');
+							sb.append(args[i]);
+						}
+						args[1] = sb.toString();
+					}
+					String name = args[1];
+
+					Placeholder[] placeholders = new Placeholder[1];
+					placeholders[0] = new Placeholder("%soundname%",args[1]);
+					this.executeCommand(name, targetarray);
+					LangOptions.playmusic_success.sendMsg(sender,placeholders);
+					return true;
+				}
+				
+			}
+			
+			Player target = server.getPlayerExact(args[0]);
+			if(target==null) {
+				LangOptions.playmusic_targetoffline.sendMsg(sender);
+				return true;
+			}
+			String[] soundnames = amusic.getPlaylistSoundnames(target.getUniqueId());
+			if(soundnames==null) {
+				LangOptions.playmusic_noplaylist.sendMsg(sender);
+				return true;
+			}
+			if(args.length>2) {
+				StringBuilder sb = new StringBuilder(args[1]);
+				for(int i = 2;i < args.length;++i) {
+					sb.append(' ');
+					sb.append(args[i]);
+				}
+				args[1] = sb.toString();
+			}
+			Placeholder[] placeholders = new Placeholder[1];
+			placeholders[0] = new Placeholder("%soundname%",args[1]);
+			for(String soundname : soundnames) {
+				if(soundname.equals(args[1])) {
+					executeCommand(args[1], target.getUniqueId());
+					LangOptions.playmusic_success.sendMsg(sender,placeholders);
+					return true;
+				}
+			}
+			LangOptions.playmusic_missingtrack.sendMsg(sender,placeholders);
+			return true;
+		} else {
+			LangOptions.playmusic_usage.sendMsg(sender);
+		}
+		return true;
+	}
+	
+	private void executeCommand(String soundname, UUID... targetuuids) {
+		if(trackable) {
+			for(UUID targetuuid : targetuuids) {
+				amusic.playSound(targetuuid,soundname);
+			}
+		} else {
+			for(UUID targetuuid : targetuuids) {
+				amusic.playSoundUntrackable(targetuuid,soundname);
+			}
+		}
+		
+	}
+
+}

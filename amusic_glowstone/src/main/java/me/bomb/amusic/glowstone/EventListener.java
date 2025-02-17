614c0da255a46bfea04ce20bf0164835941ebc03
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-17 06:43:07 +0200

    Create EventListener.java

---
+++ amusic_glowstone/src/main/java/me/bomb/amusic/glowstone/EventListener.java
@@ -0,0 +1,54 @@
+package me.bomb.amusic.glowstone;
+
+import java.net.InetAddress;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import me.bomb.amusic.PositionTracker;
+import me.bomb.amusic.glowstone.command.UploadmusicCommand;
+import me.bomb.amusic.resourceserver.ResourceManager;
+
+public final class EventListener implements Listener {
+	private final ResourceManager resourcemanager;
+	private final PositionTracker positiontracker;
+	private final ConcurrentHashMap<Object,InetAddress> playerips;
+	private final UploadmusicCommand uploadmusiccmd;
+	protected EventListener(ResourceManager resourcemanager, PositionTracker positiontracker, ConcurrentHashMap<Object,InetAddress> playerips, UploadmusicCommand uploadmusiccmd) {
+		this.resourcemanager = resourcemanager;
+		this.positiontracker = positiontracker;
+		this.playerips = playerips;
+		this.uploadmusiccmd = uploadmusiccmd;
+	}
+	@EventHandler
+	public void playerJoin(PlayerJoinEvent event) {
+		if(playerips == null) return;
+		Player player = event.getPlayer();
+		playerips.put(player, player.getAddress().getAddress());
+	}
+	@EventHandler
+	public void playerQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		UUID playeruuid = player.getUniqueId();
+		positiontracker.remove(playeruuid);
+		resourcemanager.remove(playeruuid);
+		uploadmusiccmd.logoutUploader(player);
+		if(playerips == null) return;
+		playerips.remove(player);
+	}
+	@EventHandler
+	public void playerRespawn(PlayerRespawnEvent event) {
+		positiontracker.stopMusic(event.getPlayer().getUniqueId());
+	}
+	@EventHandler
+	public void playerWorldChange(PlayerChangedWorldEvent event) {
+		positiontracker.stopMusic(event.getPlayer().getUniqueId());
+	}
+}

b19bc5df38dff8e56cd1486c2f8d12a6f587bbea
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-17 10:01:06 +0200

    Create SpongeLegacySoundStarter.java

---
+++ amusic_sponge/src/main/java/me/bomb/amusic/sponge/SpongeLegacySoundStarter.java
@@ -0,0 +1,46 @@
+package me.bomb.amusic.sponge;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.spongepowered.api.Server;
+import org.spongepowered.api.effect.sound.SoundCategories;
+import org.spongepowered.api.effect.sound.SoundType;
+import org.spongepowered.api.entity.living.player.Player;
+
+import com.flowpowered.math.vector.Vector3d;
+
+import me.bomb.amusic.SoundStarter;
+
+public final class SpongeLegacySoundStarter implements SoundStarter {
+	
+	private final Server server;
+	
+	protected SpongeLegacySoundStarter(Server server) {
+		this.server = server;
+	}
+
+	@Override
+	public void startSound(UUID uuid, short id) {
+		if(uuid == null) {
+			return;
+		}
+		Optional<Player> oplayer = server.getPlayer(uuid);
+		if(oplayer.isPresent()) {
+			Player player = oplayer.get();
+			SoundType sound = new SoundType() {
+				@Override
+				public String getName() {
+					return "amusic.music".concat(Short.toString(id));
+				}
+				@Override
+				public String getId() {
+					return "amusic.music".concat(Short.toString(id));
+				}
+			};
+			
+			player.playSound(sound, SoundCategories.VOICE, Vector3d.ZERO, 1.0E9f, 1.0f);
+		}
+	}
+
+}

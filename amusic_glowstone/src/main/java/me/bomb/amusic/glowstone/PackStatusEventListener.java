116235e6f00edd369bea49c1f3626c2f1585bf4c
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-17 06:43:43 +0200

    Create PackStatusEventListener.java

---
+++ amusic_glowstone/src/main/java/me/bomb/amusic/glowstone/PackStatusEventListener.java
@@ -0,0 +1,37 @@
+package me.bomb.amusic.glowstone;
+
+import java.util.UUID;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent.Status;
+
+import me.bomb.amusic.resourceserver.ResourceManager;
+
+public final class PackStatusEventListener implements Listener  {
+
+	private final ResourceManager resourcemanager;
+	
+	protected PackStatusEventListener(ResourceManager resourcemanager) {
+		this.resourcemanager = resourcemanager;
+	}
+	
+	@EventHandler
+	public void onResourcePackStatus(PlayerResourcePackStatusEvent event) {
+		Player player = event.getPlayer();
+		UUID uuid = player.getUniqueId();
+		Status status = event.getStatus();
+		if(status==Status.ACCEPTED) {
+			resourcemanager.setAccepted(uuid);
+			return;
+		}
+		if(status==Status.DECLINED||status==Status.FAILED_DOWNLOAD) {
+			resourcemanager.remove(uuid);
+			return;
+		}
+		if(status==Status.SUCCESSFULLY_LOADED) {
+			resourcemanager.remove(uuid); //Removes resource send if pack applied from client cache
+		}
+	}
+}

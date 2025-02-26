890fab04afd2108ce016adf06a3a1300ec2f96de
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-26 02:46:33 +0200

    Update Playing.java

--- amusic_core/src/main/java/me/bomb/amusic/Playing.java
+++ amusic_core/src/main/java/me/bomb/amusic/Playing.java
@@ -2,7 +2,7 @@ package me.bomb.amusic;
 
 public final class Playing {
 	protected final short currenttrack, maxid;
-	protected short remaining;
+	protected volatile short remaining;
 
 	protected Playing(short currenttrack, short maxid, short remaining) {
 		this.currenttrack = currenttrack;

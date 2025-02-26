be11f28a4549b22509b224dee2875b8a7d59d408
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-02-26 02:47:17 +0200

    Update LocalAMusic.java

--- amusic_core/src/main/java/me/bomb/amusic/LocalAMusic.java
+++ amusic_core/src/main/java/me/bomb/amusic/LocalAMusic.java
@@ -4,7 +4,7 @@ import java.net.InetAddress;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-import me.bomb.amusic.packedinfo.DataStorage;
+import me.bomb.amusic.packedinfo.Data;
 import me.bomb.amusic.packedinfo.SoundInfo;
 import me.bomb.amusic.resource.ResourceDispatcher;
 import me.bomb.amusic.resource.ResourceFactory;
@@ -15,21 +15,21 @@ import me.bomb.amusic.source.SoundSource;
 import me.bomb.amusic.uploader.UploadManager;
 
 public class LocalAMusic implements AMusic {
-	public final SoundSource<?> source;
+	public final SoundSource source;
 	public final PositionTracker positiontracker;
 	public final ResourceManager resourcemanager;
 	public final ResourceServer resourceserver;
 	public final ResourceDispatcher dispatcher;
-	public final DataStorage datamanager;
+	public final Data datamanager;
 	public final UploadManager uploadermanager;
 	
-	public LocalAMusic(Configuration config, SoundSource<?> source, PackSender packsender, SoundStarter soundstarter, SoundStopper soundstopper, ConcurrentHashMap<Object,InetAddress> playerips) {
+	public LocalAMusic(Configuration config, SoundSource source, PackSender packsender, SoundStarter soundstarter, SoundStopper soundstopper, ConcurrentHashMap<Object,InetAddress> playerips) {
 		this.source = source;
 		this.resourcemanager = new ResourceManager(config.packsizelimit, config.servercache, config.clientcache ? config.tokensalt : null, config.waitacception);
 		this.positiontracker = new PositionTracker(soundstarter, soundstopper);
 		this.resourceserver = new ResourceServer(config.sendpackstrictaccess ? playerips : null, config.sendpackifip, config.sendpackport, config.sendpackbacklog, resourcemanager);
 		this.dispatcher = new ResourceDispatcher(packsender, resourcemanager, positiontracker, config.sendpackhost);
-		this.datamanager = new DataStorage(config.packeddir, !config.processpack, (byte)2);
+		this.datamanager = Data.getDefault(config.packeddir.toPath(), !config.processpack);
 		this.uploadermanager = config.uploaduse ? new UploadManager(config.uploadtimeout, config.uploadlimitsize, config.uploadlimitcount, config.musicdir, config.uploadstrictaccess ? playerips : null, config.uploadifip, config.uploadport, config.uploadbacklog, config.serverfactory) : null;
 	}
 	

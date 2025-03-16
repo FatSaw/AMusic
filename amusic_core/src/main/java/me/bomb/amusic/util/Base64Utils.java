32792d82076dd8764877eef36826ed5ef66c4dc7
FatSaw <100958165+FatSaw@users.noreply.github.com>
2025-03-16 19:04:19 +0200

    Update Base64Utils.java

--- amusic_core/src/main/java/me/bomb/amusic/util/Base64Utils.java
+++ amusic_core/src/main/java/me/bomb/amusic/util/Base64Utils.java
@@ -1,9 +1,11 @@
 package me.bomb.amusic.util;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
+
+import static java.nio.charset.StandardCharsets.US_ASCII;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class Base64Utils {
 	
@@ -17,20 +19,23 @@ public final class Base64Utils {
 		base64decoder = Base64.getDecoder();
 	}
 	
+	private Base64Utils() {
+	}
+	
 	public static String toBase64Url(String name) {
-		return new String(base64urlencoder.encode(name.getBytes(StandardCharsets.UTF_8)), StandardCharsets.US_ASCII);
+		return new String(base64urlencoder.encode(name.getBytes(UTF_8)), US_ASCII);
 	}
 	
 	public static String fromBase64Url(String name) throws IllegalArgumentException {
-		return new String(base64urldecoder.decode(name.getBytes(StandardCharsets.US_ASCII)), StandardCharsets.UTF_8);
+		return new String(base64urldecoder.decode(name.getBytes(US_ASCII)), UTF_8);
 	}
 	
 	public static String toBase64(String name) {
-		return new String(base64encoder.encode(name.getBytes(StandardCharsets.UTF_8)), StandardCharsets.US_ASCII);
+		return new String(base64encoder.encode(name.getBytes(UTF_8)), US_ASCII);
 	}
 	
 	public static String fromBase64(String name) throws IllegalArgumentException {
-		return new String(base64decoder.decode(name.getBytes(StandardCharsets.US_ASCII)), StandardCharsets.UTF_8);
+		return new String(base64decoder.decode(name.getBytes(US_ASCII)), UTF_8);
 	}
 
 }

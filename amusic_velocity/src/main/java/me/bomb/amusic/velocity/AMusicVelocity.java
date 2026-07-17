8c66b1166f9cbdd2fdabdd592418de0d2b393e33
FatSaw <100958165+FatSaw@users.noreply.github.com>
2026-07-17 03:29:48 +0300

    Update AMusicVelocity.java

--- amusic_velocity/src/main/java/me/bomb/amusic/velocity/AMusicVelocity.java
+++ amusic_velocity/src/main/java/me/bomb/amusic/velocity/AMusicVelocity.java
@@ -12,20 +12,21 @@ import org.slf4j.Logger;
 import com.google.inject.Inject;
 import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.command.CommandMeta;
-import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.EventHandler;
+import com.velocitypowered.api.event.EventManager;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.connection.LoginEvent;
+import com.velocitypowered.api.event.player.PlayerResourcePackStatusEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 
-import me.bomb.amusic.AMusic;
 import me.bomb.amusic.Configuration;
 import me.bomb.amusic.GeyserHook;
 import me.bomb.amusic.LocalAMusic;
 import me.bomb.amusic.PackSender;
-import me.bomb.amusic.PositionTracker;
 import me.bomb.amusic.ServerAMusic;
-import me.bomb.amusic.resourceserver.ResourceManager;
 import me.bomb.amusic.source.LocalConvertedSource;
 import me.bomb.amusic.source.LocalUnconvertedSource;
 import me.bomb.amusic.source.MusicdirFStaticPackSource;
@@ -39,23 +40,21 @@ import me.bomb.amusic.velocity.command.LoadmusicCommand;
 import me.bomb.amusic.velocity.command.PlaymusicCommand;
 import me.bomb.amusic.velocity.command.RepeatCommand;
 import me.bomb.amusic.velocity.command.UploadmusicCommand;
+import me.bomb.amusic.velocity.event.DisconnectHandler;
+import me.bomb.amusic.velocity.event.LoginHandler;
+import me.bomb.amusic.velocity.event.PlayerResourcePackStatusHandler;
+import me.bomb.amusic.velocity.event.ProxyShutdownHandler;
 
-public final class AMusicVelocity {
-	private static AMusic instance = null;
+public final class AMusicVelocity implements EventHandler<ProxyInitializeEvent> {
 	
 	private final ProxyServer server;
-    private final Logger logger;
-	private final AMusic amusic;
-	private final ResourceManager resourcemanager;
+	private final Configuration config;
+	private final LocalAMusic amusic;
 	private final ConcurrentHashMap<Object,InetAddress> playerips;
-	private final boolean usecmd;
-	private final PositionTracker positiontracker;
-	private final String configerrors, uploaderhost, joinplaylist;
-	private GeyserHook geyserhook = null;
 	
 	@Inject
 	public AMusicVelocity(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
-		AMusicLogger.setLogger(new me.bomb.amusic.util.Logger() {
+		me.bomb.amusic.util.Logger amusiclogger = new me.bomb.amusic.util.Logger() {
 			@Override
 			public void warn(String msg) {
 				logger.warn(msg);
@@ -70,7 +69,8 @@ public final class AMusicVelocity {
 			public void error(String msg) {
 				logger.error(msg);
 			}
-		});
+		};
+		AMusicLogger.setLogger(amusiclogger);
 		Path plugindir = dataDirectory, configfile = plugindir.resolve("config.yml"), langfile = plugindir.resolve("lang.yml"), defaultresourcepackfile = plugindir.resolve("resourcepack.zip"), musicdir = plugindir.resolve("Music"), packeddir = plugindir.resolve("Packed");
 		FileSystem fs = plugindir.getFileSystem();
 		FileSystemProvider fsp = fs.provider();
@@ -79,78 +79,62 @@ public final class AMusicVelocity {
 		} catch (IOException e) {
 		}
 		boolean waitacception = true;
-		Configuration config = new Configuration(plugindir.getFileSystem(), configfile, musicdir, packeddir, waitacception, false);
-		this.configerrors = config.errors;
-		if(config.use) {
-			try {
-				fsp.createDirectory(musicdir);
-			} catch (IOException e) {
-			}
-			try {
-				fsp.createDirectory(packeddir);
-			} catch (IOException e) {
-			}
-			this.usecmd = config.usecmd;
-			this.uploaderhost = config.uploadhost;
-			this.joinplaylist = config.joinplaylist;
-			playerips = config.sendpackstrictaccess || config.uploadstrictaccess ? new ConcurrentHashMap<Object,InetAddress>(16,0.75f,1) : null;
-
-			PackSender packsender = new VelocityPackSender(server);
-	        
-			Runtime runtime = Runtime.getRuntime();
-			SoundSource soundsource = config.encoderuse ? new LocalUnconvertedSource(runtime, config.musicdir, config.packsizelimit, config.encoderbinary, config.encoderbitrate, config.encoderchannels, config.encodersamplingrate, config.packthreadcoefficient, config.packthreadlimitcount) : new LocalConvertedSource(config.musicdir, config.packsizelimit, config.packthreadcoefficient, config.packthreadlimitcount);
-			PackSource packsource = new MusicdirFStaticPackSource(new MusicdirPackSource(musicdir, config.packsizelimit), new StaticPackSource(defaultresourcepackfile, config.packsizelimit));
-			if(config.connectuse) {
-				ServerAMusic amusic = new ServerAMusic(config, soundsource, packsource, packsender, new VelocitySoundStarter(server), new VelocitySoundStopper(server), playerips == null ? null : playerips.values());
-				this.resourcemanager = amusic.resourcemanager;
-				this.positiontracker = amusic.positiontracker;
-				this.amusic = amusic;
-			} else {
-				LocalAMusic amusic = new LocalAMusic(config, soundsource, packsource, packsender, new VelocitySoundStarter(server), new VelocitySoundStopper(server), playerips == null ? null : playerips.values());
-				this.resourcemanager = amusic.resourcemanager;
-				this.positiontracker = amusic.positiontracker;
-				this.amusic = amusic;
-			}
-			this.server = server;
-	        this.logger = logger;
-			LangOptions.loadLang(new VelocityMessageSender(), langfile, false);
-			if(AMusicVelocity.instance == null) {
-				AMusicVelocity.instance = this.amusic;
-			}
-		} else {
-			this.usecmd = false;
-			this.playerips = null;
-			this.uploaderhost = null;
-			this.joinplaylist = null;
-			this.resourcemanager = null;
-			this.positiontracker = null;
+		Configuration config = new Configuration(fs, configfile, musicdir, packeddir, waitacception, false);
+		String configerrors = config.errors;
+		if(!configerrors.isEmpty()) {
+			throw new IllegalStateException("AMusic config initialization errors: \n".concat(configerrors));
+		}
+		if(!config.use) {
 			this.server = null;
-	        this.logger = null;
+			this.config = null;
 			this.amusic = null;
-		}
-    }
-	
-	public final static AMusic API() {
-		return instance;
-	}
-	
-	@Subscribe
-	public void onProxyInitialization(ProxyInitializeEvent event) {
-		if(!this.configerrors.isEmpty()) {
-			logger.error("AMusic config initialization errors: \n".concat(configerrors));
+			this.playerips = null;
 			return;
 		}
-		if(this.amusic == null) {
-			return;
+		this.server = server;
+		this.config = config;
+		try {
+			fsp.createDirectory(musicdir);
+		} catch (IOException e) {
 		}
+		try {
+			fsp.createDirectory(packeddir);
+		} catch (IOException e) {
+		}
+		this.playerips = config.sendpackstrictaccess || config.uploadstrictaccess ? new ConcurrentHashMap<Object,InetAddress>(16,0.75f,1) : null;
+
+		PackSender packsender = new VelocityPackSender(server);
+		SoundSource soundsource = config.encoderuse ? new LocalUnconvertedSource(Runtime.getRuntime(), config.musicdir, config.packsizelimit, config.encoderbinary, config.encoderbitrate, config.encoderchannels, config.encodersamplingrate, config.packthreadcoefficient, config.packthreadlimitcount) : new LocalConvertedSource(config.musicdir, config.packsizelimit, config.packthreadcoefficient, config.packthreadlimitcount);
+		PackSource packsource = new MusicdirFStaticPackSource(new MusicdirPackSource(musicdir, config.packsizelimit), new StaticPackSource(defaultresourcepackfile, config.packsizelimit));
 		
+		if(config.connectuse) {
+			ServerAMusic lamusic = new ServerAMusic(amusiclogger, config, soundsource, packsource, packsender, new VelocitySoundStarter(server), new VelocitySoundStopper(server), playerips == null ? null : playerips.values());
+			this.amusic = lamusic;
+		} else {
+			LocalAMusic lamusic = new LocalAMusic(amusiclogger, config, soundsource, packsource, packsender, new VelocitySoundStarter(server), new VelocitySoundStopper(server), playerips == null ? null : playerips.values());
+			this.amusic = lamusic;
+		}
+		LangOptions.loadLang(new VelocityMessageSender(), langfile, false);
+    }
+
+
+	@Override
+	public void execute(ProxyInitializeEvent event) {
+		this.amusic.enable();
+		GeyserHook geyser = null;
+		try {
+			geyser = new GeyserHook(this, amusic.datamanager);
+			AMusicLogger.info("Geyser hook loaded");
+		} catch (NoClassDefFoundError e) {
+		}
 		UploadmusicCommand uploadmusic = null;
-		if(this.usecmd) {
+		
+		if(config.usecmd) {
 			LoadmusicCommand loadmusic = new LoadmusicCommand(server, amusic);
 			PlaymusicCommand playmusic = new PlaymusicCommand(server, amusic, true), playmusicuntrackable = new PlaymusicCommand(server, amusic, false);
 			RepeatCommand repeat = new RepeatCommand(server, amusic);
-			uploadmusic = new UploadmusicCommand(amusic, uploaderhost);
-			CommandManager cmdmanager = this.server.getCommandManager();
+			uploadmusic = new UploadmusicCommand(amusic, config.uploadhost);
+			CommandManager cmdmanager = server.getCommandManager();
 			CommandMeta loadmusicmeta = cmdmanager.metaBuilder("loadmusic").plugin(this).build(), playmusicmeta = cmdmanager.metaBuilder("playmusic").plugin(this).build(), playmusicuntrackablemeta = cmdmanager.metaBuilder("playmusicuntrackable").plugin(this).build(), repeatmeta = cmdmanager.metaBuilder("repeat").plugin(this).build(), uploadmusicmeta = cmdmanager.metaBuilder("uploadmusic").plugin(this).build();
 			cmdmanager.register(loadmusicmeta, loadmusic);
 			cmdmanager.register(playmusicmeta, playmusic);
@@ -159,26 +143,11 @@ public final class AMusicVelocity {
 			cmdmanager.register(uploadmusicmeta, uploadmusic);
 		}
 		
-		if(this.resourcemanager != null) {
-			this.server.getEventManager().register(this, new EventListener(this.amusic, resourcemanager, positiontracker, playerips, uploadmusic, joinplaylist));
-		}
-		this.amusic.enable();
-		try {
-			this.geyserhook = new GeyserHook(this, ((LocalAMusic) this.amusic).datamanager);
-			logger.info("Geyser hook loaded");
-		} catch (NoClassDefFoundError e) {
-		}
-	}
-	
-	@Subscribe
-	public void onProxyShutdown(ProxyShutdownEvent event) {
-		if(this.geyserhook != null) {
-			this.geyserhook.unregister();
-		}
-		if(this.amusic == null) {
-			return;
-		}
-		this.amusic.disable();
+		EventManager eventmanager = server.getEventManager();
+		eventmanager.register(this, ProxyShutdownEvent.class, new ProxyShutdownHandler(amusic, geyser));
+		eventmanager.register(this, LoginEvent.class, new LoginHandler(amusic, playerips, config.joinplaylist));
+		eventmanager.register(this, DisconnectEvent.class, new DisconnectHandler(amusic, playerips, uploadmusic));
+		eventmanager.register(this, PlayerResourcePackStatusEvent.class, new PlayerResourcePackStatusHandler(amusic.resourcemanager));
 	}
 	
 }
